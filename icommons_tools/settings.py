8e98873bfa23ea5a6288114ad80aebbb3d139ba1
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-02-10 16:55:01 -0500

    fixed the static_url

--- icommons_tools/settings.py
+++ icommons_tools/settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/tools/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
