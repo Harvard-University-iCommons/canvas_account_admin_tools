c547c8b1a5114f745c3c15c1563d92b76a15c3d9
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-02-10 15:55:25 -0500

    updated settings - added the term_tool app, gunicorn; added the db config (pointing to dev for now…)

--- icommons_tools/settings.py
+++ icommons_tools/settings.py
@@ -11,12 +11,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'ISITEDEV',
+        'USER': 'coursemanager',
+        'PASSWORD': 'changeme',
+        'HOST': '',
+        'PORT': '',
+        #'HOST': 'icd3.isites.harvard.edu',
+        #'PORT': '8103',
+        'OPTIONS': {
+            'threaded': True,
+        },
     }
 }
 
@@ -24,7 +29,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -119,6 +124,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'term_tool',
+    'gunicorn',
+    'crispy_forms',
 )
 
 # A sample logging configuration. The only tangible logging
