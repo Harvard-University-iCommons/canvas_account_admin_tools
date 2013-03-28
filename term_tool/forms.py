ff557346b50b3d43b3549357dfa40081378d263d
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-03-28 08:58:57 -0400

    whitespace cleanup

--- term_tool/forms.py
+++ term_tool/forms.py
@@ -5,114 +5,114 @@ from icommons_common.models import Term,TermCode,School
 
 class EditTermForm(forms.ModelForm):
 
-	# this is a model form that's mostly automatically generated; here we specify that it should be based on the Term model:
-	class Meta:
-		model = Term
+    # this is a model form that's mostly automatically generated; here we specify that it should be based on the Term model:
+    class Meta:
+        model = Term
 
-	# make the school, term_code and academic_year fields hidden; they should not be changed once the term is created
-	school = forms.ModelChoiceField(queryset=School.objects.all(), widget=forms.widgets.HiddenInput())
-	term_code = forms.ModelChoiceField(queryset=TermCode.objects.all(), widget=forms.widgets.HiddenInput())
-	academic_year = forms.IntegerField(widget=forms.widgets.HiddenInput())
+    # make the school, term_code and academic_year fields hidden; they should not be changed once the term is created
+    school = forms.ModelChoiceField(queryset=School.objects.all(), widget=forms.widgets.HiddenInput())
+    term_code = forms.ModelChoiceField(queryset=TermCode.objects.all(), widget=forms.widgets.HiddenInput())
+    academic_year = forms.IntegerField(widget=forms.widgets.HiddenInput())
 
-	# make some additional fields required; they're not strictly required in the database, but we want them to be required here
-	start_date = forms.DateField(required=True)
-	end_date = forms.DateField(required=True)
-	xreg_start_date = forms.DateField(required=True)
-	xreg_end_date = forms.DateField(required=True)
+    # make some additional fields required; they're not strictly required in the database, but we want them to be required here
+    start_date = forms.DateField(required=True)
+    end_date = forms.DateField(required=True)
+    xreg_start_date = forms.DateField(required=True)
+    xreg_end_date = forms.DateField(required=True)
 
-	"""
-	override the clean method so that we can perform custom validation; this is done at the form level because some validation 
-	rules depend on the values of multiple fields. 
-	"""
-	def clean(self):
-		cleaned_data = super(EditTermForm, self).clean()
-		school_id = cleaned_data.get('school')
-		term_code = cleaned_data.get('term_code')
-		academic_year = cleaned_data.get('academic_year')
-		calendar_year = cleaned_data.get('calendar_year')
-		start_date = cleaned_data.get('start_date')
-		end_date = cleaned_data.get('end_date')
-		xreg_start_date = cleaned_data.get('start_date')
-		xreg_end_date = cleaned_data.get('end_date')
-		display_name = cleaned_data.get('display_name')
+    """
+    override the clean method so that we can perform custom validation; this is done at the form level because some validation 
+    rules depend on the values of multiple fields. 
+    """
+    def clean(self):
+        cleaned_data = super(EditTermForm, self).clean()
+        school_id = cleaned_data.get('school')
+        term_code = cleaned_data.get('term_code')
+        academic_year = cleaned_data.get('academic_year')
+        calendar_year = cleaned_data.get('calendar_year')
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        xreg_start_date = cleaned_data.get('start_date')
+        xreg_end_date = cleaned_data.get('end_date')
+        display_name = cleaned_data.get('display_name')
 
-		# default the display_name if it's not set
-		if display_name == None or display_name == '':
-			cleaned_data['display_name'] = '{0} {1}'.format(term_code.term_name, academic_year)
+        # default the display_name if it's not set
+        if display_name == None or display_name == '':
+            cleaned_data['display_name'] = '{0} {1}'.format(term_code.term_name, academic_year)
 
-		# make sure that the start date is before the end date
-		if xreg_start_date and xreg_end_date and start_date > end_date:
-			raise forms.ValidationError("The start date must be before the end date.")
+        # make sure that the start date is before the end date
+        if xreg_start_date and xreg_end_date and start_date > end_date:
+            raise forms.ValidationError("The start date must be before the end date.")
 
-		# make sure that the xreg start date is before the xreg end date
-		if xreg_start_date and xreg_end_date and xreg_start_date > xreg_end_date:
-			raise forms.ValidationError("The xreg start date must be before the xreg end date.")
+        # make sure that the xreg start date is before the xreg end date
+        if xreg_start_date and xreg_end_date and xreg_start_date > xreg_end_date:
+            raise forms.ValidationError("The xreg start date must be before the xreg end date.")
 
 
-		# check to make sure that the calendar_year matches the start_date
-		if calendar_year and start_date and calendar_year != start_date.year:
-			msg = u"The calendar year must match the year in the start date."
-			self._errors['calendar_year'] = self.error_class([msg])	
-			del cleaned_data['calendar_year']
+        # check to make sure that the calendar_year matches the start_date
+        if calendar_year and start_date and calendar_year != start_date.year:
+            msg = u"The calendar year must match the year in the start date."
+            self._errors['calendar_year'] = self.error_class([msg]) 
+            del cleaned_data['calendar_year']
 
-		return cleaned_data
+        return cleaned_data
 
-	
+    
 class CreateTermForm(forms.ModelForm):
 
-	# this is a model form that's mostly automatically generated; here we specify that it should be based on the Term model:
-	class Meta:
-		model = Term
+    # this is a model form that's mostly automatically generated; here we specify that it should be based on the Term model:
+    class Meta:
+        model = Term
 
-	# make the school field hidden; it's set based on the school ID that appears in the URL and shoudn't be changed by the user
-	school = forms.ModelChoiceField(queryset=School.objects.all(), widget=forms.widgets.HiddenInput())
+    # make the school field hidden; it's set based on the school ID that appears in the URL and shoudn't be changed by the user
+    school = forms.ModelChoiceField(queryset=School.objects.all(), widget=forms.widgets.HiddenInput())
 
-	# make some additional fields required; they're not strictly required in the database, but we want them to be required here
-	start_date = forms.DateField(required=True)
-	end_date = forms.DateField(required=True)
-	xreg_start_date = forms.DateField(required=True)
-	xreg_end_date = forms.DateField(required=True)
+    # make some additional fields required; they're not strictly required in the database, but we want them to be required here
+    start_date = forms.DateField(required=True)
+    end_date = forms.DateField(required=True)
+    xreg_start_date = forms.DateField(required=True)
+    xreg_end_date = forms.DateField(required=True)
 
-	"""
-	override the clean method so that we can perform custom validation; this is done at the form level because some validation 
-	rules depend on the values of multiple fields. 
-	"""
-	def clean(self):
-		cleaned_data = super(CreateTermForm, self).clean()
-		school_id = cleaned_data.get('school')
-		term_code = cleaned_data.get('term_code')
-		academic_year = cleaned_data.get('academic_year')
-		calendar_year = cleaned_data.get('calendar_year')
-		start_date = cleaned_data.get('start_date')
-		end_date = cleaned_data.get('end_date')
-		xreg_start_date = cleaned_data.get('start_date')
-		xreg_end_date = cleaned_data.get('end_date')
-		display_name = cleaned_data.get('display_name')
-
-
-		# check to see if this term already exists.
-		if Term.objects.filter(school_id=school_id.school_id, term_code=term_code.term_code, academic_year=academic_year): 
-			raise forms.ValidationError("A term record already exists for this school/year/term_code")
-
-		# default the display_name if it's not set
-		if display_name == None or display_name == '':
-			cleaned_data['display_name'] = '{0} {1}'.format(term_code.term_name, academic_year)
-
-		# make sure that the start date is before the end date
-		if start_date and end_date and start_date > end_date:
-			raise forms.ValidationError("The start date must be before the end date.")
-
-		# make sure that the xreg start date is before the xreg end date
-		if xreg_start_date and xreg_end_date and xreg_start_date > xreg_end_date:
-			raise forms.ValidationError("The xreg start date must be before the xreg end date.")
+    """
+    override the clean method so that we can perform custom validation; this is done at the form level because some validation 
+    rules depend on the values of multiple fields. 
+    """
+    def clean(self):
+        cleaned_data = super(CreateTermForm, self).clean()
+        school_id = cleaned_data.get('school')
+        term_code = cleaned_data.get('term_code')
+        academic_year = cleaned_data.get('academic_year')
+        calendar_year = cleaned_data.get('calendar_year')
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        xreg_start_date = cleaned_data.get('start_date')
+        xreg_end_date = cleaned_data.get('end_date')
+        display_name = cleaned_data.get('display_name')
 
 
-		# check to make sure that the calendar_year matches the start_date
-		if calendar_year and start_date and calendar_year != start_date.year:
-			#raise forms.ValidationError("The calendar year must match the year from the start date.")
-			msg = u"The calendar year must match the year in the start date."
-			self._errors['calendar_year'] = self.error_class([msg])	
-			del cleaned_data['calendar_year']
+        # check to see if this term already exists.
+        if Term.objects.filter(school_id=school_id.school_id, term_code=term_code.term_code, academic_year=academic_year): 
+            raise forms.ValidationError("A term record already exists for this school/year/term_code")
+
+        # default the display_name if it's not set
+        if display_name == None or display_name == '':
+            cleaned_data['display_name'] = '{0} {1}'.format(term_code.term_name, academic_year)
+
+        # make sure that the start date is before the end date
+        if start_date and end_date and start_date > end_date:
+            raise forms.ValidationError("The start date must be before the end date.")
+
+        # make sure that the xreg start date is before the xreg end date
+        if xreg_start_date and xreg_end_date and xreg_start_date > xreg_end_date:
+            raise forms.ValidationError("The xreg start date must be before the xreg end date.")
 
 
-		return cleaned_data
+        # check to make sure that the calendar_year matches the start_date
+        if calendar_year and start_date and calendar_year != start_date.year:
+            #raise forms.ValidationError("The calendar year must match the year from the start date.")
+            msg = u"The calendar year must match the year in the start date."
+            self._errors['calendar_year'] = self.error_class([msg]) 
+            del cleaned_data['calendar_year']
+
+
+        return cleaned_data
--- term_tool/views.py
+++ term_tool/views.py
@@ -26,103 +26,103 @@ class TermActionMixin(object):
 ### Function-based views:
 
 def login_view (request):
-	return HttpResponse("This is the login page!")
+    return HttpResponse("This is the login page!")
 
 def logout_view (request):
-	logout(request)
-
-	return redirect('tt:logged_out')
+    logout(request)
+    
+    return redirect('tt:logged_out')
 
 def logged_out_view (request):
-	return TemplateResponse(request, 'term_tool/logged_out.html', {})
-	return HttpResponse("This is the logged-out page!")
+    return TemplateResponse(request, 'term_tool/logged_out.html', {})
+    return HttpResponse("This is the logged-out page!")
 
 ### /Function-based views
 
 ### Class-based views:
 
 class SchoolListView(generic.ListView):
-	model = School
-	template_name = 'term_tool/school_list.html'
-	context_object_name = 'school_list'
-	login_url = reverse_lazy('tt:login')
+    model = School
+    template_name = 'term_tool/school_list.html'
+    context_object_name = 'school_list'
+    login_url = reverse_lazy('tt:login')
+    
+    # override the dispatch method in order to add the login_required decorator
+    @method_decorator(login_required(login_url=reverse_lazy('tt:login')))
+    def dispatch(self, *args, **kwargs):
+        return super(SchoolListView, self).dispatch(*args, **kwargs)
 
-	# override the dispatch method in order to add the login_required decorator
-	@method_decorator(login_required(login_url=reverse_lazy('tt:login')))
-	def dispatch(self, *args, **kwargs):
-		return super(SchoolListView, self).dispatch(*args, **kwargs)
-	
 class TermListView(generic.ListView):
-	"""
-	This view provides a list of all of the terms for a particular school.  
-	The school_id appears in the URL, and is available in the 'school_id' kwarg.
-	The term list is paginated at 12 records per page.
-	"""
-	model = Term
-	template_name = 'term_tool/term_list.html'
-	context_object_name = 'term_list'
-	paginate_by = 12
-	login_url = reverse_lazy('tt:login')
+    """
+    This view provides a list of all of the terms for a particular school.
+    The school_id appears in the URL, and is available in the 'school_id' kwarg.
+    The term list is paginated at 12 records per page.
+    """
+    model = Term
+    template_name = 'term_tool/term_list.html'
+    context_object_name = 'term_list'
+    paginate_by = 12
+    login_url = reverse_lazy('tt:login')
+    
+    # override the get_queryset method to limit the results to a particular school
+    def get_queryset(self):
+        return Term.objects.filter(school_id=self.kwargs['school_id']).order_by('-academic_year','term_code')
+    
+    # override the get_context_data method in order to add the 'school' object to the template context
+    def get_context_data(self, **kwargs):
+        context = super(TermListView, self).get_context_data(**kwargs)
+        context['school'] = School.objects.get(pk=self.kwargs['school_id'])
+        return context
+    
+    # override the dispatch method in order to add the login_required decorator
+    @method_decorator(login_required(login_url=reverse_lazy('tt:login')))
+    def dispatch(self, *args, **kwargs):
+        return super(TermListView, self).dispatch(*args, **kwargs)
 
-	# override the get_queryset method to limit the results to a particular school
-	def get_queryset(self):
-		return Term.objects.filter(school_id=self.kwargs['school_id']).order_by('-academic_year','term_code')
-
-	# override the get_context_data method in order to add the 'school' object to the template context
-	def get_context_data(self, **kwargs):
-		context = super(TermListView, self).get_context_data(**kwargs)
-		context['school'] = School.objects.get(pk=self.kwargs['school_id'])
-		return context
-
-	# override the dispatch method in order to add the login_required decorator
-	@method_decorator(login_required(login_url=reverse_lazy('tt:login')))
-	def dispatch(self, *args, **kwargs):
-		return super(TermListView, self).dispatch(*args, **kwargs)		
-	
 class TermEditView(TermActionMixin, generic.edit.UpdateView):
-	form_class = EditTermForm
-	template_name = 'term_tool/term_edit.html'
-	action = 'updated'
-	model = Term
-	context_object_name = 'term'
-	login_url = reverse_lazy('tt:login')
-
-	# override the dispatch method in order to add the login_required decorator
-	@method_decorator(login_required(login_url=reverse_lazy('tt:login')))
-	def dispatch(self, *args, **kwargs):
-		return super(TermEditView, self).dispatch(*args, **kwargs)
-
-	# override the get_success_url so that we can dynamically determine the URL to which the user should be redirected
-	def get_success_url(self):
-		return reverse('tt:termlist', kwargs={'school_id':self.object.school_id})
+    form_class = EditTermForm
+    template_name = 'term_tool/term_edit.html'
+    action = 'updated'
+    model = Term
+    context_object_name = 'term'
+    login_url = reverse_lazy('tt:login')
+    
+    # override the dispatch method in order to add the login_required decorator
+    @method_decorator(login_required(login_url=reverse_lazy('tt:login')))
+    def dispatch(self, *args, **kwargs):
+        return super(TermEditView, self).dispatch(*args, **kwargs)
+    
+    # override the get_success_url so that we can dynamically determine the URL to which the user should be redirected
+    def get_success_url(self):
+        return reverse('tt:termlist', kwargs={'school_id':self.object.school_id})
 
 class TermCreateView(TermActionMixin, generic.edit.CreateView):
-	form_class = CreateTermForm
-	template_name = 'term_tool/term_create.html'
-	action = 'created'
-	model = Term
-	login_url = reverse_lazy('tt:login')
-
-	# override the get_initial method so that we can set the school based on the school_id that appears in the URL
-	def get_initial(self):
-		initial = super(TermCreateView, self).get_initial()
-		#from pudb import set_trace; set_trace()
-		initial['school'] = self.kwargs['school_id']
-		return initial
-
-	# override the get_context_data method in order to add the 'school' object to the template context
-	def get_context_data(self, **kwargs):
-		context = super(TermCreateView, self).get_context_data(**kwargs)
-		context['school'] = School.objects.get(pk=self.kwargs['school_id'])
-		return context
-
-	# override the get_success_url so that we can dynamically determine the URL to which the user should be redirected
-	def get_success_url(self):
-		return reverse('tt:termlist', kwargs={'school_id':self.object.school_id})
-
-	# override the dispatch method in order to add the login_required decorator
-	@method_decorator(login_required(login_url=reverse_lazy('tt:login')))
-	def dispatch(self, *args, **kwargs):
-		return super(TermCreateView, self).dispatch(*args, **kwargs)
-
-		
+    form_class = CreateTermForm
+    template_name = 'term_tool/term_create.html'
+    action = 'created'
+    model = Term
+    login_url = reverse_lazy('tt:login')
+    
+    # override the get_initial method so that we can set the school based on the school_id that appears in the URL
+    def get_initial(self):
+        initial = super(TermCreateView, self).get_initial()
+        #from pudb import set_trace; set_trace()
+        initial['school'] = self.kwargs['school_id']
+        return initial
+    
+    # override the get_context_data method in order to add the 'school' object to the template context
+    def get_context_data(self, **kwargs):
+        context = super(TermCreateView, self).get_context_data(**kwargs)
+        context['school'] = School.objects.get(pk=self.kwargs['school_id'])
+        return context
+    
+    # override the get_success_url so that we can dynamically determine the URL to which the user should be redirected
+    def get_success_url(self):
+        return reverse('tt:termlist', kwargs={'school_id':self.object.school_id})
+    
+    # override the dispatch method in order to add the login_required decorator
+    @method_decorator(login_required(login_url=reverse_lazy('tt:login')))
+    def dispatch(self, *args, **kwargs):
+        return super(TermCreateView, self).dispatch(*args, **kwargs)
+        
+        
