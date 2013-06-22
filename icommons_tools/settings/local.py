a7f5a9089dac404d780c16227a0b37af2532cb99
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-06-22 11:41:49 -0400

    fix static path in local (so collectstatic doesn't do bad things in local) add newrelic (app monitoring) to requirements for test/qa/prod

--- icommons_tools/settings/local.py
+++ icommons_tools/settings/local.py
@@ -40,7 +40,7 @@ DATABASES = {
 }
 
 
-STATIC_ROOT = ''
+STATIC_ROOT = normpath(join(SITE_ROOT, 'http_static'))
 
 INSTALLED_APPS += ('debug_toolbar',)
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
