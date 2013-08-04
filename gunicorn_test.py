9090c2cf008e4791e47a213bf563bd2a44cb6429
Ubuntu <ubuntu@ip-10-0-0-40.(none)>
2013-08-04 12:12:18 +0000

    fixed gunicorn config to work with upstart

--- gunicorn_test.py
+++ gunicorn_test.py
@@ -4,9 +4,13 @@ workers = multiprocessing.cpu_count() * 2 + 1
 
 bind = "127.0.0.1:8001"
 
+'''
 daemon = True
 
 pidfile = 'gunicorn.pid'
+'''
+pythonpath = '/home/ubuntu/icommons_tools'
+
 
 accesslog = 'access.log'
 
