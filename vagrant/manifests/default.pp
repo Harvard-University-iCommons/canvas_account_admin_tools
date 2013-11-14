e522d59c5f600fc2cc2f726cb7c2bcd80a01fff9
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-11-14 07:59:19 -0500

    added vagrant stuff

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,205 @@
+# puppet manifest
+
+Exec {
+    path => [
+    '/usr/local/sbin',
+    '/usr/local/bin',
+    '/usr/sbin',
+    '/usr/bin',
+    '/sbin',
+    '/bin',
+    ],
+    logoutput => true,
+}
+
+exec {'apt-get-update':
+    command => 'apt-get update'
+}
+
+
+# make sure apache is installed and running
+package {'libxslt1-dev':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'apache':
+    name => 'apache2',
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'build-essential':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+service {'apache':
+    name => 'apache2',
+    ensure => running,
+    enable => true,
+}
+
+package {'python-dev':
+    ensure => installed,
+    require => Exec['apt-get-update']
+}
+
+package {'python-pip':
+    ensure => installed,
+    require => Package['python-dev']
+}
+
+package {'libaio1':
+    ensure => installed,
+    require => Exec['apt-get-update']
+}
+
+package {'libaio-dev':
+    ensure => installed,
+    require => Exec['apt-get-update']
+}
+
+
+package {'python-django':
+    ensure => installed,
+    require => Exec['apt-get-update'],
+}
+
+
+package {'nodejs':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'git':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'unzip':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'curl':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'libcurl4-openssl-dev':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'openssl':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'zlib1g-dev':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'libsqlite3-dev':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+package {'sqlite3':
+    ensure => latest,
+    require => Exec['apt-get-update'],
+}
+
+
+
+# make a workspace directory and link it into the homedir
+
+file {'/vagrant/workspace':
+    ensure => directory,
+    owner => 'vagrant',
+    group => 'vagrant',
+}
+
+file {'/home/vagrant/workspace':
+    ensure => link,
+    target => '/vagrant/workspace',
+}
+
+
+# install the Oracle instant client
+
+file {'/opt/oracle':
+    ensure => directory,
+}
+
+exec {'instantclient-basiclite':
+    require => [ File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-basiclite-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/BASIC_LITE_README',
+}
+
+exec {'instantclient-sqlplus':
+    require => [ File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-sqlplus-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/sqlplus',
+}
+
+exec {'instantclient-sdk':
+    require => [ File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-sdk-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/sdk',
+}
+
+file {'/opt/oracle/instantclient_11_2/libclntsh.so':
+    ensure => link,
+    target => 'libclntsh.so.11.1',
+    require => Exec['instantclient-basiclite'],
+}
+
+file {'/opt/oracle/instantclient_11_2/libocci.so':
+    ensure => link,
+    target => 'libocci.so.11.1',
+    require => Exec['instantclient-basiclite'],
+}
+
+file {'/etc/profile.d/oracle.sh':
+    ensure => file,
+    content => 'export ORACLE_HOME=/opt/oracle/instantclient_11_2; export PATH=$ORACLE_HOME:$PATH; export LD_LIBRARY_PATH=$ORACLE_HOME',
+    mode => '755',
+    require => Exec['instantclient-basiclite'],
+}
+
+exec {'install-local-cx-oracle':
+   command => '/usr/bin/pip install /vagrant/vagrant/files/cx_Oracle-5.1.2.tar.gz',
+   require => [ Package['python-pip'], File['/etc/profile.d/oracle.sh'] ],
+   creates => '/usr/local/lib/python2.7/dist-packages/cx_Oracle.so',
+   environment => [ 'ORACLE_HOME=/opt/oracle/instantclient_11_2', 'LD_LIBRARY_PATH=/opt/oracle/instantclient_11_2' ],
+}
+
+
+# install virtualenv and virtualenvwrapper - depends on pip
+
+package {'virtualenv':
+    ensure => latest,
+    provider => 'pip',
+    require => [ Package['python-pip'], ],
+}
+
+package {'virtualenvwrapper':
+    ensure => latest,
+    provider => 'pip',
+    require => [ Package['python-pip'], ],
+}
+
+file {'/etc/profile.d/venvwrapper.sh':
+    ensure => file,
+    content => 'source `which virtualenvwrapper.sh`',
+    mode => '755',
+    require => Package['virtualenvwrapper'],
+}
+
