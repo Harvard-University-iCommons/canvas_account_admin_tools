1c522bbe13597762b7801b09a4b4ad5c2223f333
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-11-21 15:57:09 -0500

    install the project requirements at provisioning time

--- vagrant/venv_bootstrap.sh
+++ vagrant/venv_bootstrap.sh
@@ -2,6 +2,6 @@
 export HOME=/home/vagrant
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv -a /home/vagrant/icommons_tools icommons_tools
+mkvirtualenv -a /home/vagrant/icommons_tools -r /home/vagrant/icommons_tools/requirements/local.txt icommons_tools 
 
 
