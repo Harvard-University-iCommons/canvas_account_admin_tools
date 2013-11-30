1834c8e30c889cb35734a10bb8e5997daa7e7421
Colin Murtaugh <cmurtaugh@gmail.com>
2013-11-30 15:12:23 -0500

    more canvas shopping functionality

--- canvas_shopping/urls.py
+++ canvas_shopping/urls.py
@@ -1,12 +1,30 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-from canvas_shopping.views import *
+from canvas_shopping import views
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'canvas_shopping.views.index'),
+    url(r'^index$', 'canvas_shopping.views.index', name='index'),
 
-    url(r'^shop$', 'canvas_shopping.views.shop'),
+    url(r'^$', views.SchoolListView.as_view(), name='schoollist'),
+
+    url(r'^(?P<school_id>\w+)$', views.CourseListView.as_view(), name='courselist'),
 
 )
 
+
+
+
+'''
+    url(
+        regex=r"^api/(?<school_id>\w+)/(?<academic_year>\d{4})/(?<term_code>\d+)/(?<course_id>\w+)/(?<user_id>\w+)$",
+        view='views.remove_shopper',
+        name='remove_shopper',
+    ),
+
+    url(
+        regex=r"^api/(?<school_id>\w+)/(?<academic_year>\d{4})/(?<term_code>\d+)/(?<course_id>\w+)$",
+        view='views.add_shopper',
+        name='add_shopper',
+    ),
+'''
--- canvas_shopping/views.py
+++ canvas_shopping/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
+#from django.views.decorators.http import require_http_methods
 
-from ims_lti_py.tool_provider import DjangoToolProvider
+#from ims_lti_py.tool_provider import DjangoToolProvider
+from icommons_common.auth.views import LoginRequiredMixin
+from icommons_common.models import School, CourseInstance
+from icommons_common.canvas_utils import *
+from django.views import generic
 
-from time import time
 import logging
 
 logger = logging.getLogger(__name__)
@@ -13,5 +16,51 @@ logger = logging.getLogger(__name__)
 
 def index(request):
 
-    return render('index.html')
+    return render(request, 'canvas_shopping/index.html')
 
+class SchoolListView(LoginRequiredMixin, generic.ListView):
+    model = School
+    template_name = 'canvas_shopping/school_list.html'
+    context_object_name = 'school_list'
+    queryset = School.objects.filter(active=1, courses__course_instances__sync_to_canvas=1)
+
+
+class CourseListView(LoginRequiredMixin, generic.ListView):
+    model = CourseInstance
+    template_name = 'canvas_shopping/course_list.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CourseListView, self).get_context_data(**kwargs)
+
+        # Get the list of courses that this user is already shopping (in Canvas)
+        shopped_course_instance_ids = []
+        enrollments = get_enrollments_by_user(self.request.user.username, 'Shopper')
+        for e in enrollments:
+            canvas_course_id = e['course_id']
+            canvas_course = get_canvas_course_by_canvas_id(canvas_course_id)
+            if canvas_course:
+                if canvas_course['sis_course_id']:
+                    shopped_course_instance_ids.append(int( canvas_course['sis_course_id']))
+
+        # Add in a QuerySet of all the courses
+        context['course_list'] = CourseInstance.objects.filter(course__school__school_id=self.kwargs['school_id'], sync_to_canvas=1)
+        context['school'] = School.objects.get(pk=self.kwargs['school_id'])
+        context['enrollments'] = enrollments
+        context['shopped_course_instance_ids'] = shopped_course_instance_ids
+        return context
+
+
+# need a view for HDS students: display the list of Canvas-mapped courses for HDS + active shopping terms
+
+def add_shopper(request):
+
+    # request must contain user_id, school, academic_year, term_code, course_code
+
+    return "some json result"
+
+def remove_shopper(request): 
+
+    # request must contain user_id, school, academic_year, term_code, course_code
+
+    return "some json result"
