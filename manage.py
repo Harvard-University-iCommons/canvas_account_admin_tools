d2f173125b111293da8ea0ae3635185ca6c8009b
Colin Murtaugh <cmurtaugh@gmail.com>
2013-12-07 15:24:29 -0500

    make the local settings the default

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icommons_tools.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icommons_tools.settings.local")
 
     from django.core.management import execute_from_command_line
 
