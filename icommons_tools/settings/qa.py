70c3715ba0ccbc839ee22cc509f380bab4f2664e
Colin Murtaugh <cmurtaugh@gmail.com>
2014-01-07 15:47:37 -0500

    added the export tool config for the qa env.

--- icommons_tools/settings/qa.py
+++ icommons_tools/settings/qa.py
@@ -141,3 +141,12 @@ BASE_ISITES_EXPORT_FILE_DOWNLOAD_URL = 'https://qa.isites.harvard.edu/exports/'
 
 GUNICORN_CONFIG = 'gunicorn_qa.py'
 
+EXPORT_TOOL = {
+    'base_file_download_url' : 'https://qa.isites.harvard.edu/exports/', 
+    'ssh_hostname' : 'icommons@qa.isites.harvard.edu', # name used to connect via ssh to perl script server
+    'ssh_private_key' : '/home/ubuntu/.ssh/id_rsa',
+    'create_site_zip_cmd' : '/u02/icommons/perlapps/iSitesAPI/scripts/export_site_files_zip.pl',
+    'remove_site_zip_cmd' : '/u02/icommons/perlapps/iSitesAPI/scripts/rm_export_file.pl',
+    'archive_cutoff_time_in_hours' : 2, # express cutoff time in hours
+    'archive_task_crontab_hours' : "*/1", # hourly frequency that periodic task executes in crontab format
+}
