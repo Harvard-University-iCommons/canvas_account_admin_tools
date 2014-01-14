5b65c265a202e64ee6a6d067c139184d83cad27d
Colin Murtaugh <cmurtaugh@gmail.com>
2014-01-14 14:42:31 -0500

    ignore the static directory - we can re-enable it if we ever need static content for this project

--- icommons_tools/settings/base.py
+++ icommons_tools/settings/base.py
@@ -88,7 +88,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    normpath(join(SITE_ROOT, 'static')),
+    #normpath(join(SITE_ROOT, 'static')),
 )
 
 
