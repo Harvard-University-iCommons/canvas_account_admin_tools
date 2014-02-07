063ff0abd81d2d23965afc77870b0233216a36ab
Colin Murtaugh <cmurtaugh@gmail.com>
2014-02-07 14:45:46 -0500

    set the ORACLE_HOME in the ssh commands

--- icommons_tools/settings/production.py
+++ icommons_tools/settings/production.py
@@ -27,8 +27,8 @@ EXPORT_TOOL = {
     'base_file_download_url': 'https://qa.isites.harvard.edu/exports/',
     'ssh_hostname': 'icommons@tool2.isites.harvard.edu',  # name used to connect via ssh to perl script server
     'ssh_private_key': '/home/icommons/.ssh/id_rsa',
-    'create_site_zip_cmd': '/u02/icommons/perl-5.12.0/bin/perl /u02/icommons/perlapps/iSitesAPI/scripts/export_site_files_zip.pl',
-    'remove_site_zip_cmd': '/u02/icommons/perl-5.12.0/bin/perl /u02/icommons/perlapps/iSitesAPI/scripts/rm_export_file.pl',
+    'create_site_zip_cmd': 'ORACLE_HOME=/u01/app/oracle/product/11.1.0 /u02/icommons/perl-5.12.0/bin/perl /u02/icommons/perlapps/iSitesAPI/scripts/export_site_files_zip.pl',
+    'remove_site_zip_cmd': 'ORACLE_HOME=/u01/app/oracle/product/11.1.0 /u02/icommons/perl-5.12.0/bin/perl /u02/icommons/perlapps/iSitesAPI/scripts/rm_export_file.pl',
     'archive_cutoff_time_in_hours': 2,  # express cutoff time in hours
     'archive_task_crontab_hours': "*/1",  # hourly frequency that periodic task executes in crontab format
     'allowed_groups': 'IcGroup:358',
