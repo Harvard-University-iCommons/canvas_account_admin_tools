9ecf80d7acbef9e8d52237d51b0c59afdd8d4058
Yip, Josie <josie_yip@harvard.edu>
2014-03-07 11:03:55 -0500

    Updated changes to add expiration_date and change the search page to have 1 field to enter either the email or ID

--- qualtrics_whitelist/views.py
+++ qualtrics_whitelist/views.py
@@ -39,7 +39,7 @@ def access_update_person(request):
     logger.debug('trying to update now/////////')
     id_update = request.POST.get('id')
     if 'cancel' in request.POST:
-        logger.warning("Now canceling and returning")
+        logger.warning("Now canceling")
     else:
         wlistSave = QualtricsAccessList()
         wlistSave.id = id_update
@@ -55,7 +55,16 @@ def access_update_person(request):
         else:
             wlistSave.description = test_description
         wlistSave.version = 0
-        # wlistSave.access_end_date = request.POST.get('access_end_date')
+        # Check the expiration_date date, if not "None", save to the database
+        # if "None", save None to the database
+        saveAs_expiration_date = request.POST.get('expiration_date')
+       
+        if saveAs_expiration_date == "None" or len(saveAs_expiration_date) == 0:
+            wlistSave.expiration_date = None
+        else:
+            wlistSave.expiration_date = saveAs_expiration_date
+       
+
         try:
             wlistSave.save()
             messages.success(request, "Whitelist update user successful")
@@ -123,22 +132,64 @@ class QualtricsAccessResultsListView(GroupMembershipRequiredMixin, generic.ListV
     def post(self, request, *args, **kwargs):
         error_message = ""
         results_list = []
-        if request.method == 'POST':
-            if 'Search' in request.POST:
-                input_user_id = request.POST.get('search_by_huid')
-                input_email = request.POST.get('search_by_email')
+        search_term = request.POST.get('user_search_term')
+        if 'Search' in request.POST:
+            if "@" in search_term:
+                # treat it as an email address     
+                personlist = Person.objects.filter(email_address__iexact=search_term)
 
-                if not len(input_user_id) <= 0:
+                if personlist:
+                    # person found on ldap_people_plus_simple model
+                    for plist in personlist:
+                        input_user_id = plist.univ_id 
+
+                        q = QualtricsAccessList.objects.all().filter(user_id=plist.univ_id)
+                        if q:
+                                
+                            for qlist in q:
+                                if plist.univ_id == qlist.user_id:
+                                    qlist.on_list = True
+                                else:
+                                    qlist.on_list = False
+                        else:
+                            # exist on Person object, but not on whitelist database
+                            qlist = QualtricsAccessList(user_id=plist.univ_id)
+                            qlist.on_list = False
+
+                        if plist.role_type_cd == 'XIDHOLDER':
+                            qlist.role_type = 'XID'
+                        else:
+                            qlist.role_type = 'HUID'
+
+                        qlist.user_id = plist.univ_id
+                        qlist.first_name = plist.name_first 
+                        qlist.last_name = plist.name_last
+                        qlist.email = plist.email_address
+                        results_list.append(qlist)
+                            
+                        return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
+                                      {'user_input': input_user_id, 'results_list': results_list, 'error_message': "", })
+                    
+                else:
+                    # person not found in Person database
+                    print "person not found in Person database"
+                    logger.error('Email not found in Person database :%s:' % search_term)
+                    return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
+                                  {'user_input': search_term, 'results_list': results_list, 'error_message': "Person not found in database", })
+
+
+            else:
+                # treat it as a user id
+                if not len(search_term) <= 0:
                     alist = []
-                    qlist = QualtricsAccessList.objects.all().filter(user_id=input_user_id)  # return all the objects from the WL table
+                    qlist = QualtricsAccessList.objects.all().filter(user_id=search_term)  # return all the objects from the WL table
 
                     if qlist:
-                        # print "User already exist on whitelist============="
+                        # User already exist on whitelist
                         for wlobj in qlist:
                             alist.append(wlobj.user_id)
 
-                        personlist = Person.objects.filter(univ_id__in=alist)
-                    
+                        personlist = Person.objects.filter(univ_id__in=alist)                    
                         for wlist in qlist:
                             for plist in personlist:
                                 if wlist.user_id == plist.univ_id:
@@ -152,8 +203,8 @@ class QualtricsAccessResultsListView(GroupMembershipRequiredMixin, generic.ListV
                                         wlist.role_type = 'HUID'
                                     results_list.append(wlist)
                     else:
-                        # print "User is not on the whitelist"
-                        personlist = Person.objects.filter(univ_id=input_user_id)
+                        # User is not on the whitelist
+                        personlist = Person.objects.filter(univ_id=search_term)
 
                         for plist in personlist:
                             plist.user_id = plist.univ_id
@@ -167,83 +218,53 @@ class QualtricsAccessResultsListView(GroupMembershipRequiredMixin, generic.ListV
                                 plist.role_type = 'HUID'
                             results_list.append(plist)  
 
-                            return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
-                                          {'user_input': input_user_id, 'results_list': results_list, 'error_message': "User id not on the whitelist.", })
-                
-                elif input_email:
-
-                    personlist = Person.objects.filter(email_address__iexact=input_email)
-
-                    if personlist:
-                        # person found on ldap_people_plus_simple model
-                        for plist in personlist:
-                            input_user_id = plist.univ_id 
-
-                            q = QualtricsAccessList.objects.all().filter(user_id=plist.univ_id)
-                            if q:
-                                
-                                for qlist in q:
-                                    if plist.univ_id == qlist.user_id:
-                                        qlist.on_list = True
-                                    else:
-                                        qlist.on_list = False
-                            else:
-                                # exist on Person object, but not on whitelist database
-                                qlist = QualtricsAccessList(user_id=plist.univ_id)
-                                qlist.on_list = False
-
-                            if plist.role_type_cd == 'XIDHOLDER':
-                                qlist.role_type = 'XID'
-                            else:
-                                qlist.role_type = 'HUID'
-                            qlist.user_id = plist.univ_id
-                            qlist.first_name = plist.name_first 
-                            qlist.last_name = plist.name_last
-                            qlist.email = plist.email_address
-                            results_list.append(qlist)
-                            
                         return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
-                                      {'user_input': input_user_id, 'results_list': results_list, 'error_message': "", })
-                    
-                    else:
-                        # person not found in Person database
-                        logger.error('Email not found in Person database :%s:' % input_email)
-                        return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
-                                      {'user_input': input_user_id, 'results_list': results_list, 'error_message': "Person not found in database", })
+                                    {'user_input': search_term, 'results_list': results_list, 'error_message': "User id not on the whitelist.", })
                                         
-            elif 'Cancel' in request.POST:
-                return HttpResponseRedirect(reverse('qwl:qualtricsaccesslist'))
+        elif 'Cancel' in request.POST:
+            return HttpResponseRedirect(reverse('qwl:qualtricsaccesslist'))
 
-            elif 'Save' in request.POST:
-                input_user_id = request.POST.get('user_id')
-                # input_list = request.POST.get('users_list')
-                input_check_list = request.POST.getlist('user_check_list')              
-                input_description = request.POST.get('description')
+        elif 'Save' in request.POST:
+            input_user_id = request.POST.get('user_id')
+            input_check_list = request.POST.getlist('user_check_list')  
 
-                # input_access_end_date = request.POST.get('access_end_date')
+            test_description = request.POST.get('description')
+            # Verify the length and truncate to max length 255, if exceeds 255 chars
+            # database max length for description is 255, but just need 150 for reason description
+            if len(test_description) > 150:
+                input_description = test_description[:150]
+                messages.info(request, "The Reason Description is too long, it has been shorten to 150 characters.")
+            else:
+                input_description = test_description
 
-                if not input_check_list:
-                    error_message = "At least one row must be checked"
-                    logger.error('Nothing checked to update')
-                    messages.error(request, "Whitelist update/deleted failed")
+            input_expiration_date = request.POST.get('expiration_date')
+            if input_expiration_date == "None" or len(input_expiration_date) == 0:
+                input_expiration_date = None
 
-                else:
-                    wlistSave = QualtricsAccessList()
+            if not input_check_list:
+                error_message = "At least one row must be checked"
+                logger.error('Nothing checked to update')
+                messages.error(request, "Whitelist update/deleted failed")
 
-                    for user in input_check_list:
-                        wlistSave.user_id = user
-                        wlistSave.description = input_description
-                        wlistSave.version = 0
-                        try:
-                            wlistSave.save()
-                            messages.success(request, "Whitelist add user successful")
-                        except IntegrityError, e:
-                            logger.error('Exception raised while saving to database:%s (%s)' % (e.args[0], type(e)))
-                            messages.error(request, "Whitelist add user failed")
-                    return HttpResponseRedirect(reverse('qwl:qualtricsaccesslist'))
+            else:
+                wlistSave = QualtricsAccessList()
+
+                for user in input_check_list:
+                    wlistSave.user_id = user
+                    wlistSave.description = input_description
+                    wlistSave.version = 0
+                    wlistSave.expiration_date = input_expiration_date
+                    try:
+                        wlistSave.save()
+                        messages.success(request, "Whitelist add user successful")
+                    except IntegrityError, e:
+                        logger.error('Exception raised while saving to database:%s (%s)' % (e.args[0], type(e)))
+                        messages.error(request, "Whitelist add user failed")
+
+            return HttpResponseRedirect(reverse('qwl:qualtricsaccesslist'))
 
         return render(request, 'qualtrics_whitelist/qualtrics_access_results_list.html', 
-                      {'user_input': input_user_id, 'results_list': results_list, 'error_message': error_message})
+                      {'user_input': search_term, 'results_list': results_list, 'error_message': error_message})
 
 
 class QualtricsAccessEditView(GroupMembershipRequiredMixin, generic.UpdateView):
