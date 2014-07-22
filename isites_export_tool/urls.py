ee817d43990448030eb69fcf24a4a8922fcfe37c
Colin Murtaugh <cmurtaugh@gmail.com>
2014-07-22 11:01:58 -0400

    extract the filename from the url

--- isites_export_tool/urls.py
+++ isites_export_tool/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns(
     url(r'^$', JobListView.as_view(), name='job_list'),
     url(r'^archive/$', JobListView.as_view(archive=True), name='job_list_archive'),
     url(r'^monitor/$', MonitorResponseView.as_view()),
-    url(r'^download_export/$', 'isites_export_tool.views.download_export_file', name='download_export_file'),
+    url(r'^download_export/(?P<export_filename>.+)$', 'isites_export_tool.views.download_export_file', name='download_export_file'),
 )
