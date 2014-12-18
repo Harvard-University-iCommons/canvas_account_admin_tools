161c54e2b94d7aeeed8ecb1aae06e4a60ee9b45f
Yates, Elliott <elliott_yates@harvard.edu>
2014-12-18 17:23:56 -0500

    adds common decorator for user id integrity check to endpoints that will be called from Canvas; removes old views.py code that were no longer being used

--- canvas_shopping/views.py
+++ canvas_shopping/views.py
@@ -1,154 +1,110 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
-from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.http import require_http_methods
-
-from icommons_common.auth.views import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import logout
-from icommons_common.models import CourseInstance
 from icommons_common.canvas_utils import *
-from django.core.cache import cache
 from django.conf import settings
-from django.http import HttpResponse
-import json
+from functools import wraps
 import logging
-from collections import defaultdict
 import re
 
 group_pattern = re.compile('LdapGroup:[a-z]+\.student')
 
 logger = logging.getLogger(__name__)
 
-# Create your views here.
 
-@login_required
-def course(request, canvas_course_id):
+def check_user_id_integrity(login_id_required=True, missing_login_id_redirect_url=None):
+    # TODO: info, debug, error logging
+    # TODO: require GET param or make it work with POST as well
+    # TODO: unit tests for missing and login required
+    # TODO: documentation
+    """
+    Decorator to check if tool user is the same as the Canvas user
+    Usage: decorate the View with check_user_id_integrity() or add arguments as needed
+           * Note the parentheses are required even when no arguments are passed,
+             as this is actually a decorator factory, not a bare decorator.
+    """
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+            cas_logout_url = "http://login.icommons.harvard.edu/pinproxy/logout"
+            # Check for 'canvas_login_id', which will be passed in by shop.js
+            # on the Canvas instance. If it's not present the code will skip this
+            # block and continue on. If it's present, verify that it matches the
+            # user_id in the tool. If there is a mismatch, send user to pin logout
+            # (this is the current security patch, maybe modified with a better solution.)
 
-    if not canvas_course_id:
-        return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, this request is invalid (missing course ID).'})
+            # canvas_login_id is the 'login_id' attribute from the Canvas
+            # user profile. It is essentially the Canvas sis_user_id (e.g. HUID)
+            canvas_login_id = request.GET.get('canvas_login_id')
+            if canvas_login_id:
+                user_id = request.user.username
+                logger.debug('user id integrity check: user in tool=%s, '
+                             'canvas_login_id from request == %s' % (user_id, canvas_login_id))
 
-    course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
+                if str(user_id) != str(canvas_login_id):
+                    logger.error('user integrity mismatch: user in tool=%s, canvas_login_id from request=%s. '
+                                 'Logging out the user from CAS' % (user_id, canvas_login_id))
+                    return redirect(cas_logout_url)
+            elif login_id_required:
+                # canvas_login_id is required to access the calling function
+                # but was not found in the request parameters; redirect to
+                # specified URL or to CAS logout if URL was not provided by
+                # calling function
+                logger.info('user integrity check: login id is required but was not found in request.')
+                if missing_login_id_redirect_url:
+                    logger.debug('user integrity check: redirecting to %s' % missing_login_id_redirect_url)
+                    return redirect(missing_login_id_redirect_url)
+                else:
+                    logger.debug('user integrity check: Logging out the user from CAS.')
+                    return redirect(cas_logout_url)
+            # User integrity check passed: tool user is the same as the Canvas user, continue
+            return view_func(request, *args, **kwargs)
+        return wrapper
+    return decorator
 
-    # is the user already in the course?
-    user_id = request.user.username
-    is_enrolled = False
-    enrollments = get_canvas_enrollment_by_user('sis_user_id:%s' % user_id)
-    if enrollments:
-        for e in enrollments:
-            logger.debug('user %s is enrolled in %d - checking against %s' % (user_id, e['course_id'], canvas_course_id))
-            if e['course_id'] == int(canvas_course_id):
-                is_enrolled = True
-                break
-
-    if is_enrolled is True:
-        # redirect the user to the actual canvas course site
-        course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
-        logger.info('User %s is already enrolled in course %s - redirecting to site.' % (user_id, canvas_course_id))
-        return redirect(course_url)
-
-    else:
-        canvas_course = get_canvas_course_by_canvas_id(canvas_course_id)
-
-        if not canvas_course:
-            # something's wrong with the course, and we can't proceed
-            logger.error('Shopping request for non-existent Canvas course id %s' % canvas_course_id)
-            return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, the Canvas course you requested does not exist.'})
-
-        # make sure that the course is available
-        if canvas_course['workflow_state'] == 'unpublished':
-            return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, this course site has not been published by the teaching staff.'})
-
-        if not canvas_course.get('sis_course_id'):
-            return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, this Canvas course is not associated with a Harvard course ID.'})
-
-        # make sure this user is eligible for shopping
-        group_ids = request.session.get('USER_GROUPS', [])
-        logger.debug("groups: " + "\n".join(group_ids))
-
-        user_can_shop = False
-        shopping_role = settings.CANVAS_SHOPPING['SHOPPER_ROLE']
-
-        # make sure this is a shoppable course and that this user can shop it
-        is_shoppable = False
-        course_instance_id = None
-        try:
-            ci = CourseInstance.objects.get(pk=canvas_course['sis_course_id'])   # TODO: prefetch term and course
-        except ObjectDoesNotExist:
-            return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, this Canvas course is associated with an invalid Harvard course ID.'})
-
-        if ci.term.shopping_active:
-            is_shoppable = True
-
-            course_instance_id = ci.course_instance_id
-
-            # any student can shop
-            for gid in group_ids:
-                if gid.startswith('ScaleSchoolEnroll:') or group_pattern.match(gid):
-                    user_can_shop = True
-
-            if user_can_shop:
-                logger.debug('User %s is eligible for shopping as a member of %s' % (user_id, gid))  
-
-            elif is_huid(user_id): 
-                logger.debug('User %s is eligible for shopping as an HUID' % user_id)
-                user_can_shop = True
-                shopping_role = settings.CANVAS_SHOPPING['VIEWER_ROLE']
-        else:
-            logger.debug('course instance term is not active for shopping: term id %d' % ci.term.term_id)
-                
-        if is_shoppable is False:
-            return render(request, 'canvas_shopping/not_shoppable.html', {'canvas_course': canvas_course})
-
-        elif user_can_shop is False:
-            return render(request, 'canvas_shopping/not_eligible.html', {'canvas_course': canvas_course})
-
-        else:
-            # Enroll this user as a shopper
-            new_enrollee = add_canvas_section_enrollee('sis_section_id:%d' % course_instance_id, shopping_role, user_id)
-            if new_enrollee:
-                # success
-                return render(request, 'canvas_shopping/successfully_added.html', {'canvas_course': canvas_course, 'course_url': course_url, 'shopping_role': shopping_role, 'settings': settings.CANVAS_SHOPPING})
-
-            else:
-                return render(request, 'canvas_shopping/error_adding.html', {'canvas_course': canvas_course})
-
-
-
-
-'''
-The course view checks to see if the authenticated user is already enrolled in the course.
-If not, and if shopping period is still active for the course, then the user will be
-enrolled in the course as a viewer.
-'''
 
 @login_required
 @require_http_methods(['GET'])
-def view_course(request, canvas_course_id):
+@check_user_id_integrity()
+def remove_shopper_role(request, canvas_course_id):
+    """
+    Remove the shopping enrollment of the current user for the specified course
+    """
+    user_id = request.user.username
 
+    course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
+
+    shopper_enrollment_id = None
+    enrollments = get_canvas_enrollment_by_user('sis_user_id:%s' % user_id)
+    if enrollments:
+        for e in enrollments:
+            if e['course_id'] == int(canvas_course_id):
+                if e['role'] == settings.CANVAS_SHOPPING['SHOPPER_ROLE']:
+                    shopper_enrollment_id = e['id']
+
+    if canvas_course_id and shopper_enrollment_id:
+        delete_canvas_enrollee_id(int(canvas_course_id), int(shopper_enrollment_id))
+        logger.debug('shopper enrollment id %s for user %s in course %s removed' % (shopper_enrollment_id, user_id, canvas_course_id))
+
+    return redirect(course_url)
+
+
+@login_required
+@require_http_methods(['GET'])
+@check_user_id_integrity()
+def view_course(request, canvas_course_id):
+    """
+    The course view checks to see if the authenticated user is already enrolled in the course.
+    If not, and if shopping period is still active for the course, then the user will be
+    enrolled in the course as a viewer.
+    """
     if not canvas_course_id:
         return render(request, 'canvas_shopping/error.html', {'error_message': 'Sorry, this request is invalid (missing course ID).'})
 
     course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
 
-    
     user_id = request.user.username
-
-    # Check for  'canvas_login_id', which  will be passed in by shop.js on the Canvas instance.  If it's not present
-    # the code will skip this block and continue on. If it's present, verify that it matches the user_id in the tool. 
-    # If there is a mismatch, send user to pin logout (this is the current security patch, maybe modified with a better solution.)  
-    if request.GET.get('canvas_login_id'): 
-        
-        #canvas_login_id the 'login_id' attribute from teh suer profile. It is essentially the sis_user_id
-        sis_user_id = request.GET.get('canvas_login_id')
-        logger.debug('user in shopping tool == %s' %user_id)
-        logger.debug('sis_user_id  from request == %s' %sis_user_id)
-
-        if str(user_id) != str(sis_user_id):
-            logger.error('user mismatch: user in shopping tool=%s, canvas user from request=%s. Logging out the user from pin' %(user_id,sis_user_id))
-            return redirect("http://login.icommons.harvard.edu/pinproxy/logout")
-
 
     # is the user already in the course?
     is_enrolled = False
@@ -185,7 +141,7 @@ def view_course(request, canvas_course_id):
 
         # TBD: check to see if the course has a public syllabus; if so, just redirect to the syllabus
 
-        # make sure this user is eligible for shopping        
+        # make sure this user is eligible for shopping
         group_ids = request.session.get('USER_GROUPS', [])
         logger.debug("groups: " + "\n".join(group_ids))
 
@@ -214,7 +170,7 @@ def view_course(request, canvas_course_id):
                 user_can_shop = True
                 shopping_role = settings.CANVAS_SHOPPING['VIEWER_ROLE']
 
-            else: 
+            else:
                 # any student can shop
                 group_ids = request.session.get('USER_GROUPS', [])
                 for gid in group_ids:
@@ -224,7 +180,7 @@ def view_course(request, canvas_course_id):
 
         else:
             logger.debug('course instance term is not active for shopping: term id %d' % ci.term.term_id)
-                
+
         if is_shoppable is False:
             return render(request, 'canvas_shopping/not_shoppable.html', {'canvas_course': canvas_course})
 
@@ -242,45 +198,6 @@ def view_course(request, canvas_course_id):
                 return render(request, 'canvas_shopping/error_adding.html', {'canvas_course': canvas_course})
 
 
-@login_required
-@require_http_methods(['GET'])
-def remove_shopper_role(request, canvas_course_id):
-    """
-    Remove the shopping enrollment of the current user for the specified course
-    """
-    user_id = request.user.username
-
-    # Check for  'canvas_login_id', which  will be passed in by shop.js on the Canvas instance.  If it's not present
-    # the code will skip this block and continue on. If it's present, verify that it matches the user_id in the tool.
-    # If there is a mismatch, send user to pin logout (this is the current security patch, maybe modified with a better solution.)
-    if request.GET.get('canvas_login_id'):
-
-        #canvas_login_id the 'login_id' attribute from the user profile. It is essentially the sis_user_id
-        sis_user_id = request.GET.get('canvas_login_id')
-        logger.debug('user in shopping tool == %s' %user_id)
-        logger.debug('sis_user_id  from request == %s' %sis_user_id)
-
-        if str(user_id) != str(sis_user_id):
-            logger.error('user mismatch: user in shopping tool=%s, canvas user from request=%s. Logging out the user from pin' %(user_id,sis_user_id))
-            return redirect("http://login.icommons.harvard.edu/pinproxy/logout")
-
-    course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
-    
-    shopper_enrollment_id = None
-    enrollments = get_canvas_enrollment_by_user('sis_user_id:%s' % user_id)
-    if enrollments:
-        for e in enrollments:
-            if e['course_id'] == int(canvas_course_id):
-                if e['role'] == settings.CANVAS_SHOPPING['SHOPPER_ROLE']:
-                    shopper_enrollment_id = e['id']
-    
-    if canvas_course_id and shopper_enrollment_id:
-        delete_canvas_enrollee_id(int(canvas_course_id), int(shopper_enrollment_id))
-        logger.debug('shopper enrollment id %s for user %s in course %s removed' % (shopper_enrollment_id, user_id, canvas_course_id))
-        
-    return redirect(course_url)
-
-
 '''
 The shop_course view checks to see if the authenticated user is already enrolled in the course.
 If not, and if shopping period is still active for the course, then the user will be
@@ -289,6 +206,7 @@ enrolled in the course as a shopper.
 
 @login_required
 @require_http_methods(['GET'])
+@check_user_id_integrity()
 def shop_course(request, canvas_course_id):
     # is the user already in the course in a non-viewer role? If so, just redirect them to the course
     # if the user is already a viewer, store that record so we can remove it later and replace it with shopper
@@ -300,20 +218,6 @@ def shop_course(request, canvas_course_id):
     course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
 
     user_id = request.user.username
-
-    # Check for  'canvas_login_id', which  will be passed in by shop.js on the Canvas instance.  If it's not present
-    # the code will skip this block and continue on. If it's present, verify that it matches the user_id in the tool.
-    # If there is a mismatch, send user to pin logout (this is the current security patch, maybe modified with a better solution.)
-    if request.GET.get('canvas_login_id'):
-
-        #canvas_login_id the 'login_id' attribute from the user profile. It is essentially the sis_user_id
-        sis_user_id = request.GET.get('canvas_login_id')
-        logger.debug('user in shopping tool == %s' %user_id)
-        logger.debug('sis_user_id  from request == %s' %sis_user_id)
-
-        if str(user_id) != str(sis_user_id):
-            logger.error('user mismatch: user in shopping tool=%s, canvas user from request=%s. Logging out the user from pin' %(user_id,sis_user_id))
-            return redirect("http://login.icommons.harvard.edu/pinproxy/logout")
 
     canvas_course = get_canvas_course_by_canvas_id(canvas_course_id)
 
@@ -377,6 +281,7 @@ it simply redirects the user to the Canvas course site.
 
 @login_required
 def course_selfreg(request, canvas_course_id):
+    # Note: course_selfreg does not need a user ID integrity check as this endpoint will not be called from Canvas
 
     course_url = '%s/courses/%s' % (settings.CANVAS_SHOPPING['CANVAS_BASE_URL'], canvas_course_id)
 
@@ -466,62 +371,9 @@ def my_list(request):
     return render(request, 'canvas_shopping/my_list.html', {'courses': courses, 'canvas_base_url': settings.CANVAS_SHOPPING.get('CANVAS_BASE_URL')})
 
 
-@login_required
-def add_shopper_ui(request):
-
-    # request must contain user_id, school, academic_year, term_code, course_code
-
-    # first, check the school/course_code/year/term to see if it's a canvas course - if not, return immediately
-    user_id = request.user.username
-    course_instance_id = request.POST.get('course_instance_id')
-    school_id = request.POST.get('school_id')
-    # from pudb import set_trace; set_trace()
-    if course_instance_id:
-        ci = CourseInstance.objects.get(pk=course_instance_id)
-        section = get_canvas_course_section(ci.course_instance_id)
-        if section is not None:
-            enrollee = add_canvas_section_enrollee(section['id'], settings.CANVAS_SHOPPING['SHOPPER_ROLE'], user_id)
-            if enrollee is not None:
-                messages.success(request, 'Successfully updated your shopping list.')
-            else:
-                messages.error(request, 'Could not update your shopping list. Please try again later')
-
-        else:
-            messages.error(request, 'There was a problem with the Canvas course.')
-            logger.error('Could not get canvas section for course_instance_id %s' % course_instance_id)
-
-    else:
-        messages.error(request, 'No course ID was sent.')
-        logger.error('add_shopper_ui was called without a course_instance_id')
-
-    next_url = reverse('sh:courselist', args=[school_id])
-    return redirect(next_url)
-
-
-@login_required
-def remove_shopper_ui(request):
-    canvas_course_id = request.POST.get('canvas_course_id')
-    canvas_enrollee_id = request.POST.get('canvas_enrollee_id')
-
-    if canvas_course_id and canvas_enrollee_id:
-        delete_canvas_enrollee_id(int(canvas_course_id), int(canvas_enrollee_id))
-        messages.success(request, 'Successfully updated your shopping list.')
-    else:
-        messages.error(request, 'Could not update your shopping list. Please try again later')
-    next_url = reverse('sh:my_list')
-    return redirect(next_url)
-
-
-# helper to build nested dicts:
-def tree():
-    return defaultdict(tree)
-
-
-
 def is_huid(id):
     # if this ID is an HUID, return true; otherwise return false
     if re.match('\d{8}$', id):
         return True
     else:
         return False
-
