3c1f63139162353ea45a97de20d96cd091547041
Yates, Elliott <elliott_yates@harvard.edu>
2015-01-13 11:18:56 -0500

    incorporates UX changes to JS, CSS, and some updates from PO on the text in my_list and removal_confirmation templates

--- canvas_shopping/shop.js
+++ canvas_shopping/shop.js
@@ -1,5 +1,3 @@
-// this is a test
-
 /*
 get the course number for the canvas course
 */
@@ -15,11 +13,9 @@ function get_course_number() {
 }
 
 var shopping_tool_url = "https://qa.tlt.harvard.edu/tools/shopping";
-//var shopping_tool_url = "https://demo.tlt.harvard.edu/tools/shopping";
-//var shopping_tool_url = "http://localhost:8001/tools/shopping";
 
 var current_user_id = ENV['current_user_id'];
-//console.log(current_user_id);
+console.log(current_user_id);
 user_url = '/api/v1/users/' + current_user_id + '/profile';
 
 /*
@@ -28,11 +24,11 @@ If so, redirect the user to the shopping tool.
 */
 
 var authorized = $('#unauthorized_message').length > 0 ? false : true
-//console.log((authorized ? '' : 'un') + 'authorized!')
+console.log((authorized ? '' : 'un') + 'authorized!')
 
 if (!authorized){
 	course_id = get_course_number();
-	//console.log('course_id = ' + course_id);
+	console.log('course_id = ' + course_id);
 	if (course_id > 0) {
 		$.getJSON(user_url, function( data ) {
 			login_id = data["login_id"].trim();
@@ -46,18 +42,18 @@ else {
 	var un = $('ul#identity > li.user_name > a').text();
 
 	if ( !un ) {
-		//console.log('-----> user is not logged in');
+		console.log('-----> user is not logged in');
 
 	} else {
-		//console.log('-----> user is logged in - username is ' + un);
+		console.log('-----> user is logged in - username is ' + un);
 		var sis_user_id = '';
 		$.getJSON(user_url, function( data ) {
 			 var sis_user_id = data["login_id"].trim();
-			 //console.log('----> '+sis_user_id);
+			 console.log('----> '+sis_user_id);
 
 			var course_id = get_course_number();
 			if (course_id > 0) {
-				//console.log('-----> course id is ' + course_id);
+				console.log('-----> course id is ' + course_id);
 				var user_enrolled = false;
 				var user_can_shop = false;
 				var is_shopper = false;
@@ -75,10 +71,10 @@ else {
 
 					var course_workflow = data['workflow_state'];
 					if(course_workflow.localeCompare('available') == 0) {
-						//console.log('course '+course_id+' is active');
+						console.log('course '+course_id+' is active');
 							var c_id = data['id'];
 							if ( course_id == c_id ) {
-								//console.log('MATCH! User is already enrolled in this course. Are they a shopper?');
+								console.log('MATCH! User is already enrolled in this course. Are they a shopper?');
 
 								// check for shopper
 								var num_enrollments = data['enrollments'].length;
@@ -86,7 +82,7 @@ else {
 								for (var n = 0; n < num_enrollments; n++) {
 									var etype = data['enrollments'][n]['type'];
 									var erole = data['enrollments'][n]['role'];
-									//console.log('Type/role is ' + etype + '/' + erole);
+									console.log('Type/role is ' + etype + '/' + erole);
 									user_enrolled = true;
 
 									if ( erole == 'Shopper' ) {
@@ -100,16 +96,16 @@ else {
 									}
 								}
 							} else {
-								//console.log('This course ' + course_id + ' does not match ' + c_id);
+								console.log('This course ' + course_id + ' does not match ' + c_id);
 							}
 
-						//console.log('-----> user enrolled: ' + user_enrolled);
+						console.log('-----> user enrolled: ' + user_enrolled);
 
 						/*
 							If the user is on the settings page, do not show the button
 						*/
 						var isNotAdminPage = ((window.location.pathname).indexOf('settings') == -1);
-						//console.log('-----> isNotAdminPage: '+isNotAdminPage);
+						console.log('-----> isNotAdminPage: '+isNotAdminPage);
 
 						if( isNotAdminPage ) {
 
@@ -120,7 +116,7 @@ else {
 							*/
 
 							if ( user_enrolled ) {
-							
+
 																/*
 									application url endpoints
 								*/
@@ -130,48 +126,49 @@ else {
 								var remove_shopper_url = shopping_tool_url + '/remove_shopper_role/' + course_and_user_id_param;
 								var remove_viewer_url  = shopping_tool_url + '/remove_viewer_role/' + course_and_user_id_param;
 								var manage_shopping_page_url = shopping_tool_url + '/my_list' + login_id;
-								
+
 								/*
 									text messages
 								*/
-								var shopping_help_doc_url = 'http://www.google.com/';
-								var shopper_message_text = 'You are a Shopper - A Shooper is... ';
-								var viewer_message_text = 'You are a viewer - A Viewer is... ';
-								var shopping_is_active_message = '<h4>Shopping is active!</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-								
+								var data_tooltip = 'More info. about access during shopping period';
+								var shopping_help_doc_url = 'https://wiki.harvard.edu/confluence/display/canvas/Course+Shopping';
+								var tooltip_link = '<a data-tooltip title="' + data_tooltip + '" target="_blank" href="' + shopping_help_doc_url + '"><i class="icon-question"></i></a>'
+								var shopper_message_text = '<h1>You have full access to this course site ' + tooltip_link + '</h1><p><em>Note: During shopping period you can access course site materials and tools that are normally restricted to the class list. Your contributions may be visible to other students and visitors to this course site. <a href="' + remove_shopper_url + '">I want to be removed.</a></em></p>';
+								var viewer_message_text = '<h1>You have limited access during shopping period ' + tooltip_link + '</h1><p>You can view this site and may receive email notifications for one day. <a href="' + remove_viewer_url + '">I want to be removed.</a></p>';
+								var participate_text = "<div class='tltmsg tltmsg-shop'><p class='participate-text'>Want to participate and continue to receive email notifications?<em>(students only)</em> <a class='btn btn-small btn-primary' href='" + add_shopper_url + "'>Get full access</a></p></div>";
+								var shopping_is_active_message = '<h1>All Harvard ID holders can view this course site during shopping period ' + tooltip_link + '</h1>In addition, all students can access course site materials and tools that are normally restricted to the class list. Student contributions maybe be visible to other students and visitors to this course site.';
+
 								var shopping_help_doc = jQuery('<a/>', {
 									id: 'shopping-help-doc',
 									href: shopping_help_doc_url,
 									target: '_blank',
 									text: 'What is this?'
 								});
-								
+
 								var shopping_banner = jQuery('<div/>', {
-									id: 'shopping-menu',
-									class: 'alert alert-block'
+									id: 'course-shopping',
+									class: 'tltmsg'
 								});
-								
+
 								var remove_shopping_role = jQuery('<a/>', {
 									id: 'remove-shopper-role',
-									class: 'btn',
 									href: remove_shopper_url,
-									text: 'Stop shopping this course'
+									text: 'I want to be removed.'
 								});
-								
+
 								var add_shopping_role = jQuery('<a/>', {
 									id: 'add-shopper-role',
-									class: 'btn',
+									class: 'btn btn-small Button Button--primary',
 									href: add_shopper_url,
-									text: 'Shop this course'
+									text: 'Get full access'
 								});
-								
+
 								var remove_viewer_role = jQuery('<a/>', {
 									id: 'remove-viewer-role',
-									class: 'btn',
 									href: remove_viewer_url,
-									text: 'Remove viewer role'
+									text: 'I want to be removed.'
 								});
-								
+
 								var manage_shopping_li_item = jQuery('<li/>', {
 									id: 'manage-shopping',
 									class: 'menu-item'
@@ -181,38 +178,39 @@ else {
 									id: 'manage-shopping-page-link',
 									class: 'menu-item-no-drop',
 									href: manage_shopping_page_url,
-									text: 'Manage Shopping'
-								});								
-								
+									text: "Courses I'm Shopping"
+								});
+
 								/* build the Manage Shopping menu item */
 								manage_shopping_li_item.append(manage_shopping_link);
-								
+
 								/*
 									for each role display the appropriate banner
 								*/
 								if ( is_shopper ) {
-									//console.log('is_shopper: true');
+									console.log('is_shopper: true');
 									$("ul#menu").append(manage_shopping_li_item);
 									shopping_banner.append(shopper_message_text);
-									shopping_banner.append(shopping_help_doc);
-									shopping_banner.append(remove_shopping_role);
+									//shopping_banner.append(shopping_help_doc);
+									//shopping_banner.append(remove_shopping_role);
 								}
 								else if ( is_viewer ) {
-									//console.log('is_viewer: true');
+									console.log('is_viewer: true');
 									$("ul#menu").append(manage_shopping_li_item);
 									shopping_banner.append(viewer_message_text);
-									shopping_banner.append(shopping_help_doc);
-									shopping_banner.append(add_shopping_role);
-									shopping_banner.append(remove_viewer_role);
+									shopping_banner.append(participate_text);
+									//shopping_banner.append(shopping_help_doc);
+									//shopping_banner.append(add_shopping_role);
+									//shopping_banner.append(remove_viewer_role);
 								} else if( is_teacher ){
-									//console.log('is_teacher: true');
+									console.log('is_teacher: true');
 									shopping_banner.append(shopping_is_active_message);
 								}
-								$('#content-wrapper').prepend(shopping_banner);
+								$('#breadcrumbs').after(shopping_banner);
 							}
 						}
 					} else {
-						//console.log('course is not published!');
+						console.log('course is not published!');
 					}
 				});
 			}
