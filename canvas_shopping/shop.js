5c646df639c539e00aa23da9ce124c7fe4df5c06
Parker, Eric P <eric_parker@harvard.edu>
2015-01-20 11:20:50 -0500

    text changes

--- canvas_shopping/shop.js
+++ canvas_shopping/shop.js
@@ -137,7 +137,7 @@ else {
 								var shopper_message_text = '<h1>You have full access to this course site ' + tooltip_link + '</h1><p><em>Note: During shopping period you can access course site materials and tools that are normally restricted to the class list. Your contributions may be visible to other students and visitors to this course site. <a href="' + remove_shopper_url + '">I want to be removed.</a></em></p>';
 								var viewer_message_text = '<h1>You have limited access during shopping period ' + tooltip_link + '</h1><p>You can view this site and may receive email notifications for one day. <a href="' + remove_viewer_url + '">I want to be removed.</a></p>';
 								var participate_text = "<div class='tltmsg tltmsg-shop'><p class='participate-text'>Want to participate and continue to receive email notifications?<em>(students only)</em> <a class='btn btn-small btn-primary' href='" + add_shopper_url + "'>Get full access</a></p></div>";
-								var shopping_is_active_message = '<h1>All Harvard ID holders can view this course site during shopping period ' + tooltip_link + '</h1>In addition, all students can access course site materials and tools that are normally restricted to the class list. Student contributions maybe be visible to other students and visitors to this course site.';
+								var shopping_is_active_message = '<h1>All Harvard ID holders can view this course site during shopping period ' + tooltip_link + '</h1>In addition, all students can access course site materials and tools that are normally restricted to the class list. Student contributions may be visible to other students and visitors to this course site.';
 
 								var shopping_help_doc = jQuery('<a/>', {
 									id: 'shopping-help-doc',
