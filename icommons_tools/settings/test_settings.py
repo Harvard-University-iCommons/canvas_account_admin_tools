9d798a617503c91fc3213917bb30933f780ebbc8
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2015-02-02 15:39:01 -0500

    Tweaks to base and test_settings file to get unit tests to run on a remote server with an empty secure settings file.  Place admins tuple directly in base like we do in ext_tools.  Comment out sites and web design from installed apps since they are not being used.  Force test_settings to be used as DJANGO_SETTINGS env variable when tests are run from the command line.

--- icommons_tools/settings/test_settings.py
+++ icommons_tools/settings/test_settings.py
@@ -1,12 +1,27 @@
-from .local import *
+from .base import *
 
-# make tests faster
-SOUTH_TESTS_MIGRATE = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-        'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+        'NAME': 'icommons_tools.db.sqlite3',
     },
 }
 
+EXPORT_TOOL = {
+    'base_file_download_url': 'https://qa.isites.harvard.edu/exports/',
+    'ssh_hostname': 'icommons@qa.isites.harvard.edu',  # name used to connect via ssh to perl script server
+    'ssh_private_key': '/home/vagrant/.ssh/id_rsa',
+    'create_site_zip_cmd': '/u02/icommons/perlapps/iSitesAPI/scripts/export_site_files_zip.pl',
+    'remove_site_zip_cmd': '/u02/icommons/perlapps/iSitesAPI/scripts/rm_export_file.pl',
+    'archive_cutoff_time_in_hours': 2,  # express cutoff time in hours
+    'archive_task_crontab_hours': "*/1",  # hourly frequency that periodic task executes in crontab format
+    'allowed_groups': 'IcGroup:358',
+    'local_archive_dir': 'logs'
+}
