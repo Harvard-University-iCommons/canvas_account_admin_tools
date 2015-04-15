fa1c379977853027d451345944813d04fb2230e1
Parker, Eric P <eric_parker@harvard.edu>
2015-04-15 16:01:32 -0400

    updated management command, simplified code

--- term_tool/management/commands/set_allow_auth_users.py
+++ term_tool/management/commands/set_allow_auth_users.py
@@ -9,11 +9,9 @@ from canvas_sdk.exceptions import CanvasAPIError
 from icommons_common.canvas_utils import SessionInactivityExpirationRC
 from icommons_common.models import (Term, CourseInstance)
 
-
 SDK_CONTEXT = SessionInactivityExpirationRC(**settings.CANVAS_SDK_SETTINGS)
 
 logger = logging.getLogger(__name__)
-
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -24,54 +22,32 @@ class Command(BaseCommand):
                     choices=['true', 'false'],
                     default='false',
                     help='allow access true or false'),
-
-        make_option('-n', '--dry-run',
-                    action='store_true',
-                    dest='dry_run',
-                    default=False,
-                    help='do no harm'),
     )
 
     def handle(self, *args, **options):
         """
-        TODO
+        Get all the terms where shopping is active
+        Then get all the courses in each term where exclude_from_shopping is False and sync_to_canvas is True
+        These are the courses we want to set is_public_to_auth_users to True
+        Cal the Canvas SDK to update each course
+        Print out how long it took to run the command
         """
 
         allow_access = options.get('allow_access')
-        print 'allow_access: %s' % allow_access
-        dry_run = options.get('dry_run')
-        verbosity = int(options.get('verbosity', 1))
-
-        if verbosity > 1:
-            logger.info('Verbosity set to %s' % verbosity)
-
-        if dry_run:
-            logger.info('Performing dry-run, no access will be set.')
-
         start_time = time.time()
 
         terms = Term.objects.filter(shopping_active=True)
-
         for term in terms:
-            logger.debug('term: %s' % term.display_name)
             account_id = 'sis_account_id:%s' % term.school_id
-
-            courses_to_process = CourseInstance.objects.filter(term=term.term_id, exclude_from_shopping=False).values('course_instance_id', 'sync_to_canvas')
-
+            courses_to_process = CourseInstance.objects.filter(term=term.term_id, exclude_from_shopping=False, sync_to_canvas=True).values('course_instance_id')
             for course in courses_to_process:
-                sync_to_canvas = course.get('sync_to_canvas')
-                if sync_to_canvas:
-                    try:
-                        '''
-                        note that this is using the updated 'update_course' SDK method
-                        '''
-                        course_id = 'sis_course_id:%s' % course.get('course_instance_id')
-
-                        resp = courses.update_course(SDK_CONTEXT, course_id, account_id, course_is_public_to_auth_users=allow_access).json()
-                        print 'id: %s, is_public_to_auth_users: %s' % (resp.get('id'), resp.get('is_public_to_auth_users'))
-                    except CanvasAPIError as api_error:
-                        logger.error("CanvasAPIError in update_course call for course_id=%s in sub_account=%s. Exception=%s:"
-                             % (course_id, account_id, api_error))
+                try:
+                    course_id = 'sis_course_id:%s' % course.get('course_instance_id')
+                    resp = courses.update_course(SDK_CONTEXT, course_id, account_id, course_is_public_to_auth_users=allow_access).json()
+                    print 'id: %s, is_public_to_auth_users: %s' % (resp.get('id'), resp.get('is_public_to_auth_users'))
+                except CanvasAPIError as api_error:
+                    logger.error("CanvasAPIError in update_course call for course_id=%s in sub_account=%s. Exception=%s:"
+                         % (course_id, account_id, api_error))
 
         end_time = time.time()
         total_time = end_time - start_time
