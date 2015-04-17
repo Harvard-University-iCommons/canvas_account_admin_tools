04e793657d78a856488f5bcedfb5ad37c29dcb75
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-04-17 13:35:48 -0400

    Updated allowed_terms list to be empty so don’t accidentally deploy wrong terms to prod

--- canvas_shopping/shop.js
+++ canvas_shopping/shop.js
@@ -1,6 +1,6 @@
 
 // allowed_terms is a whitelist of Canvas enrollment_term_ids where shopping is allowed
-var allowed_terms = ['46'];
+var allowed_terms = [];
 var shopping_tool_url = "https://icommons.harvard.edu/tools/shopping";    // the url of the shopping tool
 
 
