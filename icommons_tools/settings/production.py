b9bf6730b4f59a4edc18d6680deb7640881c5f42
Parker, Eric P <eric_parker@harvard.edu>
2015-05-02 13:15:35 -0400

    updated per PR comments.

--- icommons_tools/settings/production.py
+++ icommons_tools/settings/production.py
@@ -70,7 +70,7 @@ TERM_TOOL = {
         'IcGroup:25106': 'hbsdoc',
         'IcGroup:25178': 'sum'
     },
-    'ICOMMONS_EXT_TOOLS_URL_BASE' : 'http://isites.harvard.edu/ext_tools/canvas-course-site-wizard',
+    'ICOMMONS_EXT_TOOLS_URL_BASE' : 'https://isites.harvard.edu',
 }
 
 CANVAS_WHITELIST['canvas_url'] = 'https://harvard.instructure.com/api'
--- term_tool/views.py
+++ term_tool/views.py
@@ -125,7 +125,7 @@ class TermEditView(LoginRequiredMixin, TermActionMixin, generic.edit.UpdateView)
         '''
         encrypt user_id to place in hidden field on form
         '''
-        bulk_create_url = '%s/term/%s/bulk_create' % (settings.TERM_TOOL.get('ICOMMONS_EXT_TOOLS_URL_BASE'), self.object.term_id)
+        bulk_create_url = '/ext_tools/canvas-course-site-wizard/%s/term/%s/bulk_create' % (settings.TERM_TOOL.get('ICOMMONS_EXT_TOOLS_URL_BASE'), self.object.term_id)
         user_id = self.request.user.username
         encrypted_user = util.encrypt_string(user_id)
         context['USERID'] = encrypted_user
