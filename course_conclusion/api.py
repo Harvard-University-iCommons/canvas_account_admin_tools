303389edeb748971488623cf3b9a6e9a6defbe8d
Dave Bonner <david_bonner@harvard.edu>
2015-06-12 09:52:31 -0400

    better exception handling in api

--- course_conclusion/api.py
+++ course_conclusion/api.py
@@ -12,7 +12,6 @@ from django.views.decorators.http import require_http_methods
 from icommons_common.models import CourseInstance, School, Term
 
 
-# TODO - move marshaling to separate methods
 ISO_FORMAT = '%Y-%m-%d'
 logger = logging.getLogger(__name__)
 
@@ -66,7 +65,7 @@ def courses(request):
 
     # throw an exception if that's not a term
     term_id = request.GET['term_id']
-    get_object_or_404(Term, term_id=term_id) # TODO - include school filter?
+    get_object_or_404(Term, term_id=term_id)
 
     query = CourseInstance.objects.filter(term_id=term_id).order_by('title',
                                                                     'course_id')
@@ -92,22 +91,28 @@ def course(request, course_instance_id):
         return JsonResponse({'error': msg}, status=400)
 
     # make sure the body contains the same course instance
-    # TODO - exception handling
-    update = json.loads(request.body)
-    if update['course_instance_id'] != course_instance_id:
-        msg = 'Course instance {} in url doesn\'t match {} from body'.format(
-                  course_instance_id, update['course_instance_id'])
-        logger.error(msg)
+    try:
+        update = json.loads(request.body)
+        if update['course_instance_id'] != course_instance_id:
+            msg = 'Course instance {} in url doesn\'t match {} from body'.format(
+                      course_instance_id, update['course_instance_id'])
+            logger.error(msg)
+            return JsonResponse({'error': msg}, status=400)
+    except Exception as e:
+        msg = 'Unable to read conclude date from request body'
+        logger.exception(msg)
         return JsonResponse({'error': msg}, status=400)
 
     # now make sure the course exists
     course = get_object_or_404(CourseInstance,
                                course_instance_id=course_instance_id)
 
-    # throw an exception if the user isn't allowed to admin this school
+    # make sure the user has admin rights to this school
     if (not user_is_admin(request) and 
             course.school_id not in user_allowed_schools(request)):
-        raise PermissionDenied()
+        return JsonResponse({'error': 'Not allowed to administer {}'.format(
+                                          course.course.school.name)},
+                            status=403)
 
     # parse it into a date object
     if update['conclude_date']:
@@ -117,9 +122,19 @@ def course(request, course_instance_id):
         conclude_date = None
 
     # save the course conclude date
-    # TODO - exception handling
     course.conclude_date = conclude_date
-    course.save(update_fields=['conclude_date'])
+    try:
+        course.save(update_fields=['conclude_date'])
+    except Exception as e:
+        if conclude_date:
+            msg = 'Unable to save the new conclude date {} to'.format(
+                      conclude_date)
+        else:
+            msg = 'Unable to remove the conclude date from '
+        msg += ' course "{}"'.format(course.title)
+        logger.exception(msg)
+        return JsonResponse({'error': msg}, status=500)
+
 
     # return the same structure we do from courses()
     course_data = {
