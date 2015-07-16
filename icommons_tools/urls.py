d34059b633643f131307bf12a33954bf787d4422
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2015-07-16 15:42:37 -0400

    Remove /tools/ prefix from apps in icommons_tools since the project name is now part of the url.  Update shopping tool url used in shop.js file.

--- icommons_tools/urls.py
+++ icommons_tools/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^icommons_ui/', include('icommons_ui.urls')),
-    url(r'^tools/canvas_whitelist/', include('canvas_whitelist.urls', namespace="cwl")),
-    url(r'^tools/course_conclusion/', include('course_conclusion.urls', namespace='cc')),
-    url(r'^tools/isites_export/', include('isites_export_tool.urls', namespace="et")),
-    url(r'^tools/not_authorized/', 'icommons_ui.views.not_authorized', name="not_authorized"),
-    url(r'^tools/pin/', include('icommons_common.auth.urls', namespace="pin")),
-    url(r'^tools/qualtrics_taker_auth/', include('qualtrics_taker_auth.urls', namespace="qta")),
-    url(r'^tools/qualtrics_whitelist/', include('qualtrics_whitelist.urls', namespace="qwl")),
-    url(r'^tools/shopping/', include('canvas_shopping.urls', namespace="sh")),
-    url(r'^tools/term_tool/', include('term_tool.urls', namespace="tt")),
+    url(r'^canvas_whitelist/', include('canvas_whitelist.urls', namespace="cwl")),
+    url(r'^course_conclusion/', include('course_conclusion.urls', namespace='cc')),
+    url(r'^isites_export/', include('isites_export_tool.urls', namespace="et")),
+    url(r'^not_authorized/', 'icommons_ui.views.not_authorized', name="not_authorized"),
+    url(r'^pin/', include('icommons_common.auth.urls', namespace="pin")),
+    url(r'^qualtrics_taker_auth/', include('qualtrics_taker_auth.urls', namespace="qta")),
+    url(r'^qualtrics_whitelist/', include('qualtrics_whitelist.urls', namespace="qwl")),
+    url(r'^shopping/', include('canvas_shopping.urls', namespace="sh")),
+    url(r'^term_tool/', include('term_tool.urls', namespace="tt")),
 ]
