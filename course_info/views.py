db656dca32da677292d903863cf08a824f7d79a9
Dave Bonner <david_bonner@harvard.edu>
2015-10-08 13:16:28 -0400

    pull term codes from rest api

--- course_info/views.py
+++ course_info/views.py
@@ -26,7 +26,6 @@ def index(request):
     # to the lti launch's user.
     context = {
         'schools': _get_schools_context(canvas_user_id),
-        'terms': _get_terms_context(),
     }
     return render(request, 'course_info/index.html', context)
 
@@ -44,22 +43,3 @@ def _get_schools_context(canvas_user_id):
     schools.insert(0, {'key': 'school', 'name': 'All Schools', 'query': False,
                        'text': 'All Schools <span class="caret"></span>'})
     return json.dumps(schools)
-
-
-def _get_terms_context():
-    all_terms = get_terms()
-    terms = [{
-        'key': 'term_code',
-        'value': t['term_code'],
-        'name': t['term_name'],
-        'query': True,
-        'text': t['term_name'] + ' <span class="caret"></span>',
-                } for t in all_terms]
-    terms.sort(key=itemgetter('value'))
-    terms.insert(0, {'key': 'term_code', 'name': 'All Terms', 'query': False,
-                     'text': 'All Terms <span class="caret"></span>'})
-    return json.dumps(terms)
-
-
-def get_terms():
-    return TermCode.objects.values('term_name', 'term_code')
