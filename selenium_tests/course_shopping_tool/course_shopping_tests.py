d31ae95c835c41031133bfcd02ce01871fe2a27d
Yip, Josie <josie_yip@harvard.edu>
2015-12-22 14:32:24 -0500

    Update method name to start with ‘test’

--- selenium_tests/course_shopping_tool/course_shopping_tests.py
+++ selenium_tests/course_shopping_tool/course_shopping_tests.py
@@ -3,11 +3,11 @@ from selenium_tests.course_shopping_tool.page_objects.shopping_page_object impor
 
 class CourseShoppingTests(CourseShoppingBaseTestCase):
 
-    def shopping_page_is_loaded_test(self):
+    def test_shopping_page_is_loaded(self):
         shopping_page = CourseShoppingPageObject(self.driver)
         self.assertTrue(shopping_page.is_loaded(), 'Shopping page is not loaded, shopping is not ON')
 
-    def shopping_is_on_test(self):
+    def test_shopping_is_on(self):
         shopping_page = CourseShoppingPageObject(self.driver)
         self.assertTrue(shopping_page.is_shopping_available(), 'Shopping option is not available, shopping is not ON')
 
