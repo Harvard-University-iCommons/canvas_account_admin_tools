5eb957625f5cdea11e54b7bd49109815ad19c83d
Yates, Elliott <elliott_yates@harvard.edu>
2016-01-04 17:04:34 -0500

    refactoring of regression suite and some base test settings logic for selenium tests

--- canvas_account_admin_tools/settings/local.py
+++ canvas_account_admin_tools/settings/local.py
@@ -21,11 +21,16 @@ dictConfig(LOGGING)
 ICOMMONS_REST_API_SKIP_CERT_VERIFICATION = True
 
 SELENIUM_CONFIG = {
+   'account_admin': {
+      'relative_url': 'accounts/1/external_tools/20',
+   },
+   'canvas_base_url': CANVAS_URL,
+   'course_info_tool': {
+      'relative_url': 'accounts/1/external_tools/20',
+   },
+   'run_locally': True,
    'selenium_username': SECURE_SETTINGS.get('selenium_user'),
    'selenium_password': SECURE_SETTINGS.get('selenium_password'),
    'selenium_grid_url': SECURE_SETTINGS.get('selenium_grid_url'),
-   'canvas_base_url': CANVAS_URL,
-   'run_locally': True,
-   'account_admin_relative_url': 'accounts/1/external_tools/20',
-   'course_info_tool_relative_url': 'accounts/1/external_tools/20'
+   'use_htmlrunner': SECURE_SETTINGS.get('selenium_use_htmlrunner', True),
 }
--- selenium_tests/course_info/course_info_base_test_case.py
+++ selenium_tests/course_info/course_info_base_test_case.py
@@ -7,29 +7,21 @@ from django.conf import settings
 
 
 class CourseInfoBaseTestCase(BaseSeleniumTestCase):
-    """
-    Bulk Create base test case, all other tests will subclass this class
-    """
     @classmethod
     def setUpClass(cls):
-        """
-        setup values for the tests
-        """
         super(CourseInfoBaseTestCase, cls).setUpClass()
 
-        driver = cls.driver
         cls.USERNAME = settings.SELENIUM_CONFIG.get('selenium_username')
         cls.PASSWORD = settings.SELENIUM_CONFIG.get('selenium_password')
+
         cls.CANVAS_BASE_URL = settings.SELENIUM_CONFIG.get('canvas_base_url')
-        cls.TOOL_RELATIVE_URL = settings.SELENIUM_CONFIG.get('course_info_tool_relative_url')
+        cls.TOOL_RELATIVE_URL = settings.SELENIUM_CONFIG['course_info_tool']['relative_url']
         cls.TOOL_URL = urljoin(cls.CANVAS_BASE_URL, cls.TOOL_RELATIVE_URL)
 
-        cls.course_info_parent_page = AccountAdminDashboardPage(driver)
+        cls.course_info_parent_page = AccountAdminDashboardPage(cls.driver)
         cls.course_info_parent_page.get(cls.TOOL_URL)
-        login_page = PinLoginPageObject(driver)
+        login_page = PinLoginPageObject(cls.driver)
         if login_page.is_loaded():
-            print " logging the user in"
             login_page.login_xid(cls.USERNAME, cls.PASSWORD)
         else:
             print '(User {} already logged in to PIN)'.format(cls.USERNAME)
-            
