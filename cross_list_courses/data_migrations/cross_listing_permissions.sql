28e8506d427dc59aad2f52c1f65f9a62b04ef6cd
Sapna Mysore <sapna_mysore@harvard.edu>
2016-02-12 11:20:10 -0500

    One more :-) , Added reverse SQL

--- cross_list_courses/data_migrations/cross_listing_permissions.sql
+++ cross_list_courses/data_migrations/cross_listing_permissions.sql
@@ -1,9 +1,11 @@
-INSERT INTO lti_permissions_ltipermission
- (permission, school_id, canvas_role, allow)
- VALUES('cross_listing', '*', 'Account Admin',TRUE);
 
- INSERT INTO lti_permissions_ltipermission
- (permission, school_id, canvas_role, allow)
- VALUES('cross_listing', '*', 'AccountAdmin',TRUE);
- 
+
+INSERT INTO lti_permissions_ltipermission (permission, school_id, canvas_role, allow) VALUES
+('cross_listing', '*', 'AccountAdmin', TRUE),
+('cross_listing', '*', 'Account Admin', TRUE);
+
+
+-- reverse SQL:
+DELETE  FROM lti_permissions_ltipermission WHERE permission="cross_listing";
+
 
