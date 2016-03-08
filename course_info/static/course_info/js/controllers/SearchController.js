7e5585cb7742ab0f1538404b818de460c81c26c2
Sapna Mysore <sapna_mysore@harvard.edu>
2016-03-08 10:49:06 -0500

    Modified the confusing comment :-)

--- course_info/static/course_info/js/controllers/SearchController.js
+++ course_info/static/course_info/js/controllers/SearchController.js
@@ -156,7 +156,7 @@
                             $scope.searchInProgress = true;
                         });
                         $scope.enableColumnSorting(false);
-                        //filter the sites flagged to be excluded(exclude_from_isites set to 1)
+                        //filter the sites flagged to be excluded(get only ones with exclude_from_isites set to 0)
                         var queryParameters = {
                             exclude_from_isites: 0};
                         if ($scope.queryString.trim() != '') {
