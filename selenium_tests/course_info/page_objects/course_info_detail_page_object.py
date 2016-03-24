d610f0f231a68eeb176eb0ca92a72f895813d2f8
Yates, Elliott <elliott_yates@harvard.edu>
2016-03-24 11:36:39 -0400

    tlt-2523: edit course details -- tlt-2376 selenium test stubs
    - initial stubs for course details page object and selenium test cases for tlt-2376

---
+++ selenium_tests/course_info/page_objects/course_info_detail_page_object.py
@@ -0,0 +1,10 @@
+from selenium_tests.course_info.page_objects.course_info_base_page_object \
+    import CourseInfoBasePageObject
+
+
+class Locators(object):
+    pass
+
+
+class CourseInfoDetailPageObject(CourseInfoBasePageObject):
+    pass
