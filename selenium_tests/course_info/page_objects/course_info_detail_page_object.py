9ded6232528a672decc604d00cd8eecf70a7abef
Yates, Elliott <elliott_yates@harvard.edu>
2016-03-24 21:07:42 -0400

    tlt-2523: edit course details selenium tests (tlt-2376) -- fixes flakiness in reset form test
    - values in form input elements are fetched asynchronously, so we need to be sure the values are loaded before attempting to access the form input elements

--- selenium_tests/course_info/page_objects/course_info_detail_page_object.py
+++ selenium_tests/course_info/page_objects/course_info_detail_page_object.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 from selenium_tests.course_info.page_objects.course_info_base_page_object \
     import CourseInfoBasePageObject
@@ -22,11 +23,13 @@ class CourseInfoDetailPageObject(CourseInfoBasePageObject):
     page_loaded_locator = Locators.MAIN_TAG
 
     def enter_text_in_input_field(self, field_name, value):
+        self._wait_for_input_field_to_be_visible(field_name)
         input = self.find_element(*Locators.INPUT_BY_FIELD_NAME(field_name))
         input.clear()
         input.send_keys(value)
 
     def get_input_field_value(self, field_name):
+        self._wait_for_input_field_to_be_visible(field_name)
         input = self.find_element(*Locators.INPUT_BY_FIELD_NAME(field_name))
         return input.get_attribute('value')
 
@@ -35,3 +38,10 @@ class CourseInfoDetailPageObject(CourseInfoBasePageObject):
 
     def reset_form(self):
         self.find_element(*Locators.RESET_FORM_BUTTON).click()
+
+    def _wait_for_input_field_to_be_visible(self, field_name):
+        # only want to access input elements once the values are loaded from the
+        # server (which in this case means the spinner disappears and the input
+        # element becomes visible)
+        WebDriverWait(self._driver, 10).until(lambda s: s.find_element(
+            *Locators.INPUT_BY_FIELD_NAME(field_name)).is_displayed())
