99953e25f889ef79d54599bf848f3c48e8a63865
Sapna Mysore <sapna_mysore@harvard.edu>
2016-03-28 15:31:40 -0400

    Cleaned up some comments.

--- selenium_tests/course_info/course_info_details_noneditable_course_tests.py
+++ selenium_tests/course_info/course_info_details_noneditable_course_tests.py
@@ -11,7 +11,7 @@ class CourseInfoDetailsNonEditTests(CourseInfoBaseTestCase):
 
     def setUp(self):
         super(CourseInfoDetailsNonEditTests, self).setUp()
-        #Load a non-ILE course to validate non editable courses
+        # Load a non-ILE course to validate non editable courses
         self._load_test_course()
         self.assertTrue(self.detail_page.is_loaded())
 
@@ -26,14 +26,15 @@ class CourseInfoDetailsNonEditTests(CourseInfoBaseTestCase):
         self.assertEquals(self.detail_page.is_locator_element_present
                           (Locators.SAVE_FORM_BUTTON), False)
 
+        # The tests are validating against an exact match for the class
+        # attribute of non-editable fields
+        # (Editable fields also contain a "ng-binding" but it is always
+        # accompanied by additional suffixes "ng-binding some_value",
+        # which is not an exact match).
 
-        """TLT-2523: Every field has a span/input id regardless of whether
-        the field is editable, so tests cannot rely on the presence of an
-        span/input id.  The tests are validating against a class
-        element for non-editable fields
-        """
+        expected_class_name = "ng-binding"
 
-        # NOTE: The span element exists only there is a value in the field.
+        # The span element exists only there if is a value in the field.
         # Limitation:  Each of the fields of the test site is
         # populated with a value. When testing a new course, note that
         # tests may break if field values are null.
@@ -52,9 +53,9 @@ class CourseInfoDetailsNonEditTests(CourseInfoBaseTestCase):
         ]
 
         # loops through each of the non-editable fields and looks for the
-        # unique class element associated with non-editable fields and also
-        # verifies that the field is not rendered as an input
-        expected_class_name = "ng-binding"
+        # exact class associated with non-editable fields and also
+        # verifies that the field is not rendered as an input element
+
         for element in non_editable_fields:
             self.assertEqual(self.detail_page.get_span_element_class(element),
                              expected_class_name)
