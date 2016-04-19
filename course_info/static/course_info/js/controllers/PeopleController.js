c2bb49efd59be50915f2b1548b570b75afe005ed
Yates, Elliott <elliott_yates@harvard.edu>
2016-04-19 13:31:37 -0400

    tlt-2119: course info add people -- improved loading behavior / UI feedback while loading

--- course_info/static/course_info/js/controllers/PeopleController.js
+++ course_info/static/course_info/js/controllers/PeopleController.js
@@ -265,7 +265,7 @@
                 });
         };
         $scope.disableAddToCourseButton = function(){
-            return ($scope.operationInProgress || $scope.searchTerms.length == 0);
+            return ($scope.operationInProgress || ($scope.searchTerms.length == 0));
         };
         $scope.filterSearchResults = function(searchResults){
             var filteredResults = Array();
@@ -612,6 +612,7 @@
         $scope.clearMessages();  // initialize user-facing messages
         $scope.confirmRemoveModalInstance = null;
         $scope.courseInstanceId = $routeParams.courseInstanceId;
+        $scope.initialCourseMembersFetched = false;  // UI component visibility
 
         $scope.roles = [
             // NOTE - these may need to be updated based on the db values
@@ -660,23 +661,27 @@
                     method: 'GET',
                     data: queryParams,
                     dataSrc: 'data',
-                    dataType: 'json',
-                    success: function(data, textStatus, jqXHR) {
-                        callback({
-                            recordsTotal: data.count,
-                            recordsFiltered: data.count,
-                            data: data.results,
-                        });
-                    },
-                    error: function(data, textStatus, errorThrown) {
-                        $log.error('Error getting data from ' + url + ': '
-                                   + textStatus + ', ' + errorThrown);
-                        callback({
-                            recordsTotal: 0,
-                            recordsFiltered: 0,
-                            data: [],
-                        });
-                    },
+                    dataType: 'json'
+                }).done(function dataTableGetDone(data, textStatus, jqXHR) {
+                    callback({
+                        recordsTotal: data.count,
+                        recordsFiltered: data.count,
+                        data: data.results,
+                    });
+                })
+                .fail(function dataTableGetFail(data, textStatus, errorThrown) {
+                    $log.error('Error getting data from ' + url + ': '
+                               + textStatus + ', ' + errorThrown);
+                    callback({
+                        recordsTotal: 0,
+                        recordsFiltered: 0,
+                        data: [],
+                    });
+                })
+                .always(function dataTableGetAlways() {
+                    // notify UI to stop showing a loading... message
+                    $scope.initialCourseMembersFetched = true;
+                    $scope.$digest();
                 });
             },
             createdRow: function( row, data, dataIndex ) {
@@ -692,8 +697,10 @@
                     next: '',
                     previous: '',
                 },
+                processing: 'Loading, please wait...'
             },
             lengthMenu: [10, 25, 50, 100],
+            processing: true,
             // yes, this is a deprecated param.  yes, it's still required.
             // see https://datatables.net/forums/discussion/27287/using-an-ajax-custom-get-function-don-t-forget-to-set-sajaxdataprop
             sAjaxDataProp: 'data',
