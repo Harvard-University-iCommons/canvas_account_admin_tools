c2da38eea42331250fc830b4c29ba2e757de41bc
hktest <hiukei_chow@harvard.edu>
2016-04-22 17:33:49 -0400

    Move the td text search services into selenium_common (even though it's not used yet for cross-listing tool, but will be). see link: https://github.com/Harvard-University-iCommons/selenium_common/tree/task%2Fhchow%2F2581_moving_td_text_search_to_common

--- selenium_tests/cross_listing/page_objects/cross_listing_base_page_object.py
+++ selenium_tests/cross_listing/page_objects/cross_listing_base_page_object.py
@@ -1,34 +1,9 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
+"""This is the base_page object for page object that is specific to the
+cross-listing tool"""
+
 
 from selenium_common.base_page_object import BasePageObject
 
 
-class Locators(object):
-    @classmethod
-    def TD_TEXT_XPATH(cls, search_text):
-        """
-        Returns a locator for the first table cell element in any table on the
-        page whose text matches `search_text`. Note: matches text of element
-        (not inclusive of child-elements) with leading and trailing space
-        stripped and whitespace normalized between words.
-        """
-        return By.XPATH, "//td[normalize-space(text())='{}']".format(
-            search_text)
-
-
 class CrossListingBasePageObject(BasePageObject):
-    def __init__(self, driver):
-        super(CrossListingBasePageObject, self).__init__(driver)
-        self.focus_on_tool_frame()
-
-    def get_cell_with_text(self, search_text):
-        """
-        :return: The text in the web element in the course code column
-                 else return None
-        """
-
-        try:
-            return self.find_element(*Locators.TD_TEXT_XPATH(search_text))
-        except NoSuchElementException:
-            return None
+    pass
