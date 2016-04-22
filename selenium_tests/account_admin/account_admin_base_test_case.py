d596936f32cecd9b051ea09c1026ebdca3a7dbbe
hktest <hiukei_chow@harvard.edu>
2016-04-22 11:28:40 -0400

    Fixed per PR feedback. Using SELENIUM_CONFIG[‘key'] instead of get.

--- selenium_tests/account_admin/account_admin_base_test_case.py
+++ selenium_tests/account_admin/account_admin_base_test_case.py
@@ -12,14 +12,14 @@ class AccountAdminBaseTestCase(BaseSeleniumTestCase):
     def setUpClass(cls):
         super(AccountAdminBaseTestCase, cls).setUpClass()
 
-        cls.USERNAME = settings.SELENIUM_CONFIG.get('selenium_username')
-        cls.PASSWORD = settings.SELENIUM_CONFIG.get('selenium_password')
-        cls.CANVAS_BASE_URL = settings.SELENIUM_CONFIG.get('canvas_base_url')
+        cls.USERNAME = settings.SELENIUM_CONFIG['selenium_username']
+        cls.PASSWORD = settings.SELENIUM_CONFIG['selenium_password']
+        cls.CANVAS_BASE_URL = settings.SELENIUM_CONFIG['canvas_base_url']
         cls.TOOL_RELATIVE_URL = settings.SELENIUM_CONFIG['account_admin']['relative_url']
         cls.TOOL_URL = urljoin(cls.CANVAS_BASE_URL, cls.TOOL_RELATIVE_URL)
 
         cls.acct_admin_dashboard_page = AccountAdminDashboardPage(cls.driver)
-        cls.acct_admin_dashboard_page.get(cls.TOOL_URL)
+        cls.acct_admin_dashboard_page[cls.TOOL_URL]
         login_page = PinLoginPageObject(cls.driver)
         if login_page.is_loaded():
             login_page.login_xid(cls.USERNAME, cls.PASSWORD)
