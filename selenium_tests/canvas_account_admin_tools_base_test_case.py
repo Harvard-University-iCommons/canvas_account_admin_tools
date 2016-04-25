fca0bbebe606d1dba40faea7140b44065f13fc99
hktest <hiukei_chow@harvard.edu>
2016-04-25 17:04:42 -0400

    - Created a base_test_case at the project level, so all the apps in this project can re-use the setUpClass.  The setup method will still be app-dependent. - Updated cross_listing_base_test_case to use the setupClass at the project.

---
+++ selenium_tests/canvas_account_admin_tools_base_test_case.py
@@ -0,0 +1,29 @@
+from urlparse import urljoin
+
+from django.conf import settings
+
+from selenium_common.base_test_case import BaseSeleniumTestCase
+from selenium_common.pin.page_objects.pin_login_page_object import PinLoginPageObject
+from selenium_tests.account_admin.page_objects.account_admin_dashboard_page_object import AccountAdminDashboardPage
+
+
+class AccountAdminBaseTestCase(BaseSeleniumTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(AccountAdminBaseTestCase, cls).setUpClass()
+
+        cls.USERNAME = settings.SELENIUM_CONFIG['selenium_username']
+        cls.PASSWORD = settings.SELENIUM_CONFIG['selenium_password']
+        cls.CANVAS_BASE_URL = settings.SELENIUM_CONFIG['canvas_base_url']
+        cls.TOOL_RELATIVE_URL = settings.SELENIUM_CONFIG['account_admin']['relative_url']
+        cls.TOOL_URL = urljoin(cls.CANVAS_BASE_URL, cls.TOOL_RELATIVE_URL)
+
+        cls.acct_admin_dashboard_page = AccountAdminDashboardPage(cls.driver)
+        cls.acct_admin_dashboard_page.get(cls.TOOL_URL)
+        login_page = PinLoginPageObject(cls.driver)
+        if login_page.is_loaded():
+            login_page.login_xid(cls.USERNAME, cls.PASSWORD)
+        else:
+            print '(User {} already logged in to PIN)'.format(cls.USERNAME)
+
