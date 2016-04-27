c2c0cb0602a71b6272b3118781b6ef57aa5fd636
hktest <hiukei_chow@harvard.edu>
2016-04-27 14:49:48 -0400

    Cleaned up STUBS, and added DDT.

--- selenium_tests/cross_listing/add_cross_listing_tests.py
+++ selenium_tests/cross_listing/add_cross_listing_tests.py
@@ -1,39 +1,39 @@
-from selenium_tests.cross_listing.cross_listing_base_test_case import \
-    CrossListingBaseTestCase
-from selenium_tests.cross_listing.page_objects.cross_listing_main_page import\
-    MainPageObject
+from ddt import ddt, data, unpack
 
+from selenium_common.base_test_case import get_xl_data
 from selenium_common.base_page_object import BasePageObject
 
+from selenium_tests.cross_listing.cross_listing_base_test_case import \
+    CrossListingBaseTestCase, TEST_DATA_CROSS_LISTING_MAPPINGS
 
+
+@ddt
 class AddCrossListingTests(CrossListingBaseTestCase):
 
-    """TLT-2589, AC: TBD """
+    """TLT-2589, AC #1 and 7
+    This test adds a valid cross-list pairing to the cross-listing table
+    """
 
-    def test_adding_a_cross_listing_pairing(self, course_instance_1,
-                                            course_instance_2):
+    @data(get_xl_data(TEST_DATA_CROSS_LISTING_MAPPINGS))
+    @unpack
+    def test_adding_a_cross_listing_pairing(self, test_case_id,
+                                            primary_cid, secondary_cid,
+                                            expected_result):
 
-        """This test adds a valid cross-list pairing to the cross-listing table
-        Removal of mapping will be covered in a future story. Cleanup
-        via UI or rest API (?) required.
-        """
-        # TODO: This test should not go in until remove is in place
+        # TODO: This is a test stub only:
 
-        # TODO:  Think about data, perhaps valid and invalid data can just be
-        #  in one spreadsheet, but test is conditioned against some text?
-
-        course_instance1 = self.test_data(get_test_data_from_local or xls)
-        course_instance2 = self.test_data(get_test_data_from_local or xls)
-
-        # STUB:
         # Adds the pairing
         self.assertTrue(
-                self.index_page.add_cross_listing_pairing())
+                self.index_page.add_cross_listing_pairing(primary_cid,
+                                                          secondary_cid))
+
         # Verifies alert confirmation appears on page
         self.assertTrue(
                 self.index_page.confirm_presence_of_some_alert_box())
-        # Verifies that that course is added successfully or failed,
-        # based on either search by ID or by confirmation messasge only
-        self.assertTrue(
-                self.index_page._confirm_alert_text()
 
+        # Verifies that that course is added successfully or failed,
+        # based on either search by ID or by confirmation text only - TBD
+
+        self.assertTrue(
+                self.index_page._confirm_based_on_alert_text_or_search_by_text_on_page()
+        # TODO:  based on expected result?
