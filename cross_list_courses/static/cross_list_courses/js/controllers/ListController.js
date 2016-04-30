d2da5944b380afb07b1a09a9e6d66436e2609869
Yates, Elliott <elliott_yates@harvard.edu>
2016-04-29 23:20:13 -0400

    tlt-1314: crosslisting - improves form validation, prepares frontend for integration with backend - current code just gives a random response (success, general failure, specific fake backend failure) - indications in code for how to hook up to working backend

--- cross_list_courses/static/cross_list_courses/js/controllers/ListController.js
+++ cross_list_courses/static/cross_list_courses/js/controllers/ListController.js
@@ -3,135 +3,188 @@
      * Angular controller for the cross listing page.
      */
     var app = angular.module('CrossListCourses');
-    app.controller('ListController',
-        ['$scope', '$http', '$timeout', '$document', '$window', '$compile','djangoUrl','$log',
-        function($scope, $http, $timeout, $document, $window, $compile, $djangoUrl, $log){
+    app.controller('ListController', ListController);
 
-            $scope.dtInstance = null;
+    function ListController($scope, $http, $timeout, $document, $window,
+                            $compile, djangoUrl, $log, $q) {
+        $scope.message = null;
+        $scope.operationInProgress = false;
+        $scope.rawFormInput = {primary: '', secondary: ''};
+
+        $scope.cleanCourseInstanceInput = function (courseInstanceString) {
+            return courseInstanceString.trim();
+        };
+        $scope.clearMessages = function () {
             $scope.message = null;
-            $scope.operationInProgress = false;
-            // todo: strip()
-            $scope.primaryCourseInput = '';
-            $scope.secondaryCourseInput = '';
+        };
+        $scope.formatCourse = function (course_instance) {
+            return course_instance.course.school_id.toUpperCase() +
+                ' ' + course_instance.course.registrar_code +
+                '-' + course_instance.term.display_name +
+                '-' + course_instance.course_instance_id;
+        };
+        $scope.handleAjaxError = function (data, status, headers, config, statusText) {
+            $log.error('Error attempting to ' + config.method + ' ' + config.url +
+                ': ' + status + ' ' + statusText + ': ' + JSON.stringify(data));
+        };
+        $scope.handleAjaxErrorResponse = function (r) {
+            // angular promise then() function returns a response object,
+            // unpack for our old-style error handler
+            $scope.handleAjaxError(
+                r.data, r.status, r.headers, r.config, r.statusText);
+        };
+        $scope.invalidInput = function () {
+            return (!$scope.isValidCourseInstance($scope.rawFormInput.primary) || !$scope.isValidCourseInstance($scope.rawFormInput.secondary))
+                || ($scope.cleanCourseInstanceInput($scope.rawFormInput.primary) ==
+                $scope.cleanCourseInstanceInput($scope.rawFormInput.secondary))
+        };
+        $scope.isValidCourseInstance = function (courseInstanceString) {
+            return $scope.cleanCourseInstanceInput(courseInstanceString)
+                .match(/^[0-9]+$/);
+        };
+        $scope.postNewCrosslisting = function (primary, secondary) {
+            var url = djangoUrl.reverse('icommons_rest_api_proxy',
+                ['api/course/v2/xlist_maps/']);
 
-            $scope.formatCourse = function(course_instance) {
-                return course_instance.course.school_id.toUpperCase() +
-                                ' ' + course_instance.course.registrar_code +
-                                '-' + course_instance.term.display_name +
-                                '-' + course_instance.course_instance_id;
-            };
+            // todo: this is the call to make, switch when backend is ready
+            // return $http.post(url,
+            //     {primary: primary, secondary: secondary});
 
-            $scope.invalidInput = function() {
-                return !$scope.isValidCourseInstance($scope.primaryCourseInput) ||
-                        !$scope.isValidCourseInstance($scope.secondaryCourseInput)
-            };
+            // temporarily return a random success, failure, or backend msg
+            var deferred = $q.defer();
+            $timeout(function () {
+                return Math.round(Math.random())
+                    ? deferred.resolve({})
+                    : Math.round(Math.random())
+                    ? deferred.reject({error: {detail: 'specific error detail from the backend'}})
+                    : deferred.reject({});
+            }, 1000);
+            return deferred.promise;
+        };
+        $scope.submitAddCrosslisting = function () {
+            $scope.clearMessages();
+            $scope.operationInProgress = true;
 
-            $scope.isValidCourseInstance = function(courseInstanceString) {
-                return courseInstanceString.match(/[0-9]+/);
-            };
+            var primary = $scope.cleanCourseInstanceInput(
+                $scope.rawFormInput.primary);
+            var secondary = $scope.cleanCourseInstanceInput(
+                $scope.rawFormInput.secondary);
 
-            $scope.submitAddCrosslisting = function() {
-                $scope.clearMessages();
-                $scope.operationInProgress = true;
-                // todo: make the call, process it
-                $timeout(function() {
+            $scope.postNewCrosslisting(primary, secondary)
+                .then(function postSucceeded(response) {
                     $scope.message = {
                         alertType: 'success',
-                        text: $scope.primaryCourseInput + ' was successfully crosslisted with ' + $scope.secondaryCourseInput + '.'
+                        text: primary + ' was successfully crosslisted with ' +
+                        secondary + '.'
                     };
-                    $scope.operationInProgress = false;
-                }, 1000);
-                // todo: reload datatable
-            };
+                    $scope.dtInstance.reloadData();
+                }, function postFailed(response) {
+                    // todo: enable once hooked up to backend
+                    // $scope.handleAjaxErrorResponse(response);
+                    var errorText = '';
+                    if (((response || {}).error || {}).detail) {
+                        errorText = response.error.detail;
+                    } else {
+                        errorText = primary + ' could not be ' +
+                            'crosslisted with ' + secondary + ' at this ' +
+                            'time. Please check the course instance IDs ' +
+                            'and try again.';
+                    }
+                    $scope.message = {alertType: 'danger', text: errorText};
+                }).finally(function cleanupAfterPost() {
+                $scope.operationInProgress = false;
+            });
+        };
 
-            $scope.clearMessages = function() {
-                $scope.message = null;
-            };
+        /**
+         * Datatable setup and helpers
+         */
 
-            $scope.dtOptions = {
-                ajax: function(data, callback, settings) {
-                    var url = $djangoUrl.reverse('icommons_rest_api_proxy',
-                                                   ['api/course/v2/xlist_maps/']);
+        $scope.dtInstance = null;
 
-                    var queryParams = {
-                        offset: data.start,
-                        limit: data.length,
-                        ordering:'-primary_course_instance__term__calendar_year,primary_course_instance__term__school_id',
-                        include: 'course_instance',
-                    };
+        function renderCourseInstance(courseInstance) {
+            if (courseInstance) {
+                return $scope.formatCourse(courseInstance);
+            } else {
+                return 'N/A';
+            }
+        }
 
-                    $.ajax({
-                        url: url,
-                        method: 'GET',
-                        data: queryParams,
-                        dataSrc: 'data',
-                        dataType: 'json',
-                        success: function(data, textStatus, jqXHR) {
-                            callback({
-                                recordsTotal: data.count,
-                                recordsFiltered: data.count,
-                                data: data.results,
-                            });
-                        },
-                        error: function(data, textStatus, errorThrown) {
-                            $log.error('Error getting data from ' + url + ': '
-                                       + textStatus + ', ' + errorThrown);
-                            callback({
-                                recordsTotal: 0,
-                                recordsFiltered: 0,
-                                data: [],
-                            });
-                        },
-                    });
-                },
-                language: {
-                    emptyTable: 'There are no cross listed courses to display.',
-                    info: 'Showing _START_ to _END_ of _TOTAL_ course mappings',
-                    infoEmpty: 'Showing 0 to 0 of 0 course mappings',
-                    paginate: {
-                        next: '',
-                        previous: '',
+        $scope.dtOptions = {
+            ajax: function (data, callback, settings) {
+                var url = djangoUrl.reverse('icommons_rest_api_proxy',
+                    ['api/course/v2/xlist_maps/']);
+
+                var queryParams = {
+                    offset: data.start,
+                    limit: data.length,
+                    ordering: '-primary_course_instance__term__calendar_year,primary_course_instance__term__school_id',
+                    include: 'course_instance',
+                };
+
+                $.ajax({
+                    url: url,
+                    method: 'GET',
+                    data: queryParams,
+                    dataSrc: 'data',
+                    dataType: 'json',
+                    success: function (data, textStatus, jqXHR) {
+                        callback({
+                            recordsTotal: data.count,
+                            recordsFiltered: data.count,
+                            data: data.results,
+                        });
                     },
-                    processing: '<img src="' + window.globals.STATIC_URL + 'images/ajax-loader-large.gif" class="loading"/> Loading...',
-                },
-                lengthMenu: [10, 25, 50, 100],
-                // yes, this is a deprecated param.  yes, it's still required.
-                // see https://datatables.net/forums/discussion/27287/using-an-ajax-custom-get-function-don-t-forget-to-set-sajaxdataprop
-                sAjaxDataProp: 'data',
-                searching: false,
-                serverSide: true,
-                processing:true,
-                ordering: false,
-            };
-
-            $scope.dtColumns = [
-                {
-                    data: null,
-                    render: function(data, type, row) {
-                        if (data.primary_course_instance) {
-                             return $scope.formatCourse(data.primary_course_instance);
-                        } else {
-                            return 'N/A';
-                        }
+                    error: function (data, textStatus, errorThrown) {
+                        $log.error('Error getting data from ' + url + ': '
+                            + textStatus + ', ' + errorThrown);
+                        callback({
+                            recordsTotal: 0,
+                            recordsFiltered: 0,
+                            data: [],
+                        });
                     },
-                    title: 'Primary',
-                    bSortable: false,
+                });
+            },
+            language: {
+                emptyTable: 'There are no cross listed courses to display.',
+                info: 'Showing _START_ to _END_ of _TOTAL_ course mappings',
+                infoEmpty: 'Showing 0 to 0 of 0 course mappings',
+                paginate: {
+                    next: '',
+                    previous: '',
                 },
-                {
-                    data: null,
-                    render: function(data, type, full, meta) {
-                        //TODO: explore refactoring the render code that is
-                        // duplicated for both columns
-                        if (data.secondary_course_instance) {
-                            return $scope.formatCourse(data.secondary_course_instance);
+                processing: '<i class="fa fa-refresh fa-spin"></i> Loading...',
+            },
+            lengthMenu: [10, 25, 50, 100],
+            // yes, this is a deprecated param.  yes, it's still required.
+            // see https://datatables.net/forums/discussion/27287/using-an-ajax-custom-get-function-don-t-forget-to-set-sajaxdataprop
+            sAjaxDataProp: 'data',
+            searching: false,
+            serverSide: true,
+            processing: true,
+            ordering: false,
+        };
 
-                        } else {
-                            return 'N/A';
-                        }
-                    },
-                    title: 'Secondary',
+        $scope.dtColumns = [
+            {
+                data: null,
+                render: function (data) {
+                    return renderCourseInstance(
+                        data.primary_course_instance);
                 },
-            ];
+                title: 'Primary',
+                bSortable: false,
+            },
+            {
+                data: null,
+                render: function (data) {
+                    return renderCourseInstance(
+                        data.secondary_course_instance);
+                },
+                title: 'Secondary',
+            },
+        ];
 
-        }]);
+    }
 })();
