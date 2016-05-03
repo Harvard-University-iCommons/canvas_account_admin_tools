7fdeb35f82aa873f35cbd8ea935b8b885688bc2e
hktest <hiukei_chow@harvard.edu>
2016-05-03 14:03:37 -0400

    - Cleaned up code after debug with unpacking is addressed - Got success logic working - Error logic is in progress.

--- selenium_tests/cross_listing/add_cross_listing_tests.py
+++ selenium_tests/cross_listing/add_cross_listing_tests.py
@@ -5,52 +5,54 @@ from selenium_common.base_test_case import get_xl_data
 from selenium_tests.cross_listing.cross_listing_base_test_case import \
     CrossListingBaseTestCase, TEST_DATA_CROSS_LISTING_MAPPINGS
 
-#     TLT-2589, AC #1 and 7
-#     This test adds a valid cross-list pairing to the cross-listing table
-
 @ddt
 class AddCrossListingTests(CrossListingBaseTestCase):
 
     @data(*get_xl_data(TEST_DATA_CROSS_LISTING_MAPPINGS))
     @unpack
-    def test_add_cross_listing_pairing(self, test_case_id,
+    def test_add_cross_listing_pairing(self, test_case,
                                        primary_cid,
                                        secondary_cid,
                                        expected_result):
 
-        self.driver.save_screenshot("before_add_pairing.png")
+        """
+        TLT-2589, AC #1 and 7
+        This test adds a valid cross-list pairing to the cross-listing table
+        """
+        # This adds the pairing to the cross-list table and clicks submit
+        self.main_page.add_cross_listing_pairing(primary_cid, secondary_cid)
 
-        # self.index_page.add_cross_listing_pairing('123456', '234567')
-        # Debug
-        print test_case_id, primary_cid, secondary_cid, expected_result
-
-        self.index_page.add_cross_listing_pairing(primary_cid,
-                                                  secondary_cid)
-        self.driver.save_screenshot("after_add.png")
-
-
-        # Verifies alert confirmation appears
+        # This verifies that the confirmation box appears
         self.assertTrue(
-                self.index_page.confirm_presence_of_confirmation_alert()
+                self.main_page.confirm_presence_of_confirmation_alert()
         )
 
-        # Verifies the cids are on page if it's successfully cross-listed
+        #  Verifies a successful cross-list add
         if expected_result == 'success':
-            self.assertTrue(self.index_page.is_add_successful())
+            expected_text = "{} was successfully crosslisted with {}.".format(
+                primary_cid, secondary_cid)
+            actual_text = self.main_page.get_confirmation_text()
+            # Verifies that the expected success message matches the actual
+            # success confirmation message
+            self.assertEqual(actual_text, expected_text,
+                             "Error. Expected success message is '{}' but "
+                             "message is returning '{}'".format(expected_text,
+                                                                actual_text))
+
 
         # Verifies error message if cross-listing is unsuccessful
         if expected_result == 'fail':
-            expected_text = "EXPECTED ERROR MESSAGE"  # TODO:need error
+            expected_text = 'could not be crosslisted'
             actual_text = self.index_page.get_confirmation_text()
             self.assertEqual(actual_text, expected_text,
                              "Error. Expected error is '{}' but error is "
                              "returning '{}'".format(expected_text,
                                                      actual_text))
 
-        # test_data must contain a value in expected result column.
+        #  Verifies test_data spreadsheet contains a value in the "expected
+        #  result" column
         else:
             raise ValueError('given_access column for expected result {} must '
                              'be either '
-                             '\'fail\' or \'success\''.format(expected_result)
-                             )
-        
+                             '\'fail\' or \'success\''.format(expected_result))
+
