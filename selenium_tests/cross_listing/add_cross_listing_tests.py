fc0cc37ccc2d150c945499601b30e742d63f511f
hktest <hiukei_chow@harvard.edu>
2016-05-04 16:02:06 -0400

    Fixed - working test now on unsuccessful add

--- selenium_tests/cross_listing/add_cross_listing_tests.py
+++ selenium_tests/cross_listing/add_cross_listing_tests.py
@@ -5,6 +5,7 @@ from selenium_common.base_test_case import get_xl_data
 from selenium_tests.cross_listing.cross_listing_base_test_case import \
     CrossListingBaseTestCase, TEST_DATA_CROSS_LISTING_MAPPINGS
 
+
 @ddt
 class AddCrossListingTests(CrossListingBaseTestCase):
 
@@ -14,7 +15,6 @@ class AddCrossListingTests(CrossListingBaseTestCase):
                                        primary_cid,
                                        secondary_cid,
                                        expected_result):
-
         """
         TLT-2589, AC #1 and 7
         This test adds a valid cross-list pairing to the cross-listing table
@@ -27,7 +27,7 @@ class AddCrossListingTests(CrossListingBaseTestCase):
                 self.main_page.confirm_presence_of_confirmation_alert()
         )
 
-        #  Verifies a successful cross-list add
+        # Verifies a successful cross-list add
         if expected_result == 'success':
             expected_text = "{} was successfully crosslisted with {}.".format(
                 primary_cid, secondary_cid)
@@ -39,20 +39,13 @@ class AddCrossListingTests(CrossListingBaseTestCase):
                              "message is returning '{}'".format(expected_text,
                                                                 actual_text))
 
-
         # Verifies error message if cross-listing is unsuccessful
         if expected_result == 'fail':
-            expected_text = 'could not be crosslisted'
-            actual_text = self.index_page.get_confirmation_text()
-            self.assertEqual(actual_text, expected_text,
-                             "Error. Expected error is '{}' but error is "
-                             "returning '{}'".format(expected_text,
-                                                     actual_text))
+            self.assertTrue(self.main_page.verify_error_elements_are_present())
 
-        #  Verifies test_data spreadsheet contains a value in the "expected
-        #  result" column
+        #  Verifies test_data spreadsheet contains a 'fail' or 'success'
+        #  value in the "expected result" column; cannot be empty
         else:
             raise ValueError('given_access column for expected result {} must '
                              'be either '
                              '\'fail\' or \'success\''.format(expected_result))
-
