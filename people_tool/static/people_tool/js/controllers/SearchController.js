c62034928f3738d340deaa9b6c908415f48300c0
Yates, Elliott <elliott_yates@harvard.edu>
2016-06-17 11:50:05 -0400

    tlt-2490: people tool - shows no record count footer when the datatable is empty (relies on the emptyTable message only to convey that information)

--- people_tool/static/people_tool/js/controllers/SearchController.js
+++ people_tool/static/people_tool/js/controllers/SearchController.js
@@ -121,7 +121,7 @@
             language: {
                 emptyTable: 'There are no people to display.',
                 info: 'Showing _START_ to _END_ of _TOTAL_ people',
-                infoEmpty: 'Showing 0 to 0 of 0 people',
+                infoEmpty: '',
                 paginate: {
                     next: '',
                     previous: ''
