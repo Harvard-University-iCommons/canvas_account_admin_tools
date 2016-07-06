4533734046b110d420c571dd7af01d813e72b7ee
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-07-06 10:04:34 -0400

    Changed initial permissions to False.

--- people_tool/migrations/0001_initial.py
+++ people_tool/migrations/0001_initial.py
@@ -5,25 +5,26 @@ from django.db import migrations
 
 LTI_PERMISSIONS_DATA = [
     ('people_tool', '*', 'Account Observer', False),
-	('people_tool', '*', 'AccountAdmin', True),
-	('people_tool', '*', 'Account Admin', True),
-	('people_tool', '*', 'Course Head', False),
-	('people_tool', '*', 'Course Support Staff', False),
-	('people_tool', '*', 'Department Admin', False),
-	('people_tool', '*', 'DesignerEnrollment', False),
-	('people_tool', '*', 'Faculty', False),
-	('people_tool', '*', 'Guest', False),
-	('people_tool', '*', 'Harvard-Viewer', False),
-	('people_tool', '*', 'Help Desk', False),
-	('people_tool', '*', 'Librarian', False),
-	('people_tool', '*', 'ObserverEnrollment', False),
-	('people_tool', '*', 'SchoolLiaison', True),
-	('people_tool', '*', 'Shopper', False),
-	('people_tool', '*', 'StudentEnrollment', False),
-	('people_tool', '*', 'TaEnrollment', False),
-	('people_tool', '*', 'TeacherEnrollment', False),
-	('people_tool', '*', 'Teaching Staff', False)
+    ('people_tool', '*', 'AccountAdmin', False),
+    ('people_tool', '*', 'Account Admin', False),
+    ('people_tool', '*', 'Course Head', False),
+    ('people_tool', '*', 'Course Support Staff', False),
+    ('people_tool', '*', 'Department Admin', False),
+    ('people_tool', '*', 'DesignerEnrollment', False),
+    ('people_tool', '*', 'Faculty', False),
+    ('people_tool', '*', 'Guest', False),
+    ('people_tool', '*', 'Harvard-Viewer', False),
+    ('people_tool', '*', 'Help Desk', False),
+    ('people_tool', '*', 'Librarian', False),
+    ('people_tool', '*', 'ObserverEnrollment', False),
+    ('people_tool', '*', 'SchoolLiaison', False),
+    ('people_tool', '*', 'Shopper', False),
+    ('people_tool', '*', 'StudentEnrollment', False),
+    ('people_tool', '*', 'TaEnrollment', False),
+    ('people_tool', '*', 'TeacherEnrollment', False),
+    ('people_tool', '*', 'Teaching Staff', False)
 ]
+
 
 def create_lti_permissions(apps, schema_editor):
     LtiPermission = apps.get_model('lti_permissions', 'LtiPermission')
@@ -32,14 +33,16 @@ def create_lti_permissions(apps, schema_editor):
     for permission in LTI_PERMISSIONS_DATA:
         LtiPermission.objects.create(**dict(zip(fields, permission)))
 
+
 def reverse_permissions_load(apps, schema_editor):
     LtiPermission = apps.get_model('lti_permissions', 'LtiPermission')
     LtiPermission.objects.filter(permission='people_tool').delete()
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-         ('lti_permissions', '0001_initial'),
+        ('lti_permissions', '0001_initial'),
     ]
 
     operations = [
