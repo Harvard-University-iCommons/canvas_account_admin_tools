1a101e00ca337f4c04e41fec672cecc74d40b026
hktest <hiukei_chow@harvard.edu>
2016-11-21 18:40:46 -0500

    Cleaned up permissions test

--- selenium_tests/bulk_create/bulk_create_permission_tests.py
+++ selenium_tests/bulk_create/bulk_create_permission_tests.py
@@ -3,15 +3,15 @@ from ddt import ddt, data, unpack
 from selenium_common.base_test_case import get_xl_data
 from selenium_common.canvas.canvas_masquerade_page_object \
     import CanvasMasqueradePageObject
-from selenium_tests.bulk_create.bulk_create_base_test_case \
-    import BulkCreateBaseTestCase
+from selenium_tests.account_admin.account_admin_base_test_case \
+    import AccountAdminBaseTestCase
 from selenium_tests.bulk_create.bulk_create_base_test_case \
     import CANVAS_PERMISSION_ROLES
 from selenium_tests.bulk_create.page_objects.index_page import IndexPageObject
 
 
 @ddt
-class BulkCreatePermissionTests(BulkCreateBaseTestCase):
+class BulkCreatePermissionTests(AccountAdminBaseTestCase):
     @data(*get_xl_data(CANVAS_PERMISSION_ROLES))
     @unpack
     def test_roles_access(self, user_id, given_access):
@@ -22,14 +22,14 @@ class BulkCreatePermissionTests(BulkCreateBaseTestCase):
         """
 
         masquerade_page = CanvasMasqueradePageObject(self.driver,
-                                                     self.canvas_base_url)
+                                                     self.CANVAS_BASE_URL)
         index_page = IndexPageObject(self.driver)
 
         # Masquerade as a user defined in the Test_Data/Roles Access spreadsheet
         masquerade_page.masquerade_as(user_id)
 
-        # Once masqueraded as user, go to Course Create Tool
-        self.driver.get(self.base_url)
+        # Once masqueraded as user, go back to Account Admin Console
+        self.driver.get(self.TOOL_URL)
 
         if given_access == 'no':
             print "Verifying user %s is denied access to course bulk create " \
@@ -40,9 +40,12 @@ class BulkCreatePermissionTests(BulkCreateBaseTestCase):
         elif given_access == 'yes':
             print "Verifying user %s is granted access to course bulk create " \
                   "tool" % user_id
-            self.assertTrue(index_page.is_authorized(),
-                            'User {} unexpectedly denied access'.format(
-                                user_id))
+
+            # Clicks into Canvas Site Create tool
+            self.acct_admin_dashboard_page.select_create_canvas_site_link()
+
+            # Verifies that the user can click into the bulk create tool
+            self.assertTrue(index_page.is_loaded())
 
         else:
             raise ValueError('given_access column for user {} must be either '
