ee66bf5ae87a6ac71540173342e79495d0a1df30
Yates, Elliott <elliott_yates@harvard.edu>
2017-01-06 22:17:53 -0500

    tlt-1805: publish courses - metrics for bulk operations and rq worker timeout handling - completed jobs now log metrics - rq worker timeouts no longer swallowed; they now halt execution of the bulk_publish_canvas_sites job function (which is still allowed to mark as complete with failure status)

--- publish_courses/async_operations.py
+++ publish_courses/async_operations.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.conf import settings
+from rq import get_current_job
 
 from async.models import Process
 from bulk_utilities.bulk_course_settings import BulkCourseSettingsOperation
@@ -15,8 +16,16 @@ logger = logging.getLogger(__name__)
 
 def bulk_publish_canvas_sites(process_id, account=None, course_list=None,
                               term=None, published=True, dry_run=False):
-    logger.info("Starting bulk_publish_canvas_sites job for "
-                "process_id:{}".format(process_id))
+    logger.info("Starting bulk_publish_canvas_sites job {}".format(process_id))
+
+    try:
+        job = get_current_job()
+        logger.debug("RQ job details: {}".format(job.to_dict()))
+    except Exception as e:
+        logger.exception(
+            "Failed to get current job information from RQ for process_id: {}. "
+            "(Possibly running bulk_publish_canvas_sites() outside of an RQ "
+            "worker?)".format(process_id))
 
     try:
         process = Process.objects.get(id=process_id)
@@ -40,17 +49,14 @@ def bulk_publish_canvas_sites(process_id, account=None, course_list=None,
     try:
         op.execute()
     except Exception as e:
-        process.state = Process.COMPLETE
+        logger.exception("bulk_publish_canvas_sites job {} "
+                         "failed".format(process_id))
         process.status = 'failed'
         process.details['error'] = str(e)
-        process.details['stats'] = op.get_stats_dict()
-        process.save(update_fields=['state', 'status', 'details'])
-        raise
 
     process.details['stats'] = op.get_stats_dict()
     process.state = Process.COMPLETE
-    process.save(update_fields=['state', 'details'])
+    process.save(update_fields=['state', 'status', 'details'])
 
-    logger.info("Finished bulk_publish_canvas_sites job for "
-                "process_id:{}".format(process_id))
+    logger.info("Finished bulk_publish_canvas_sites job {}".format(process_id))
     return process
