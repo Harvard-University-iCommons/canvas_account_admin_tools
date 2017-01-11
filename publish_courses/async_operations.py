9ff79cf77bacf82b91dbf284da9e010ef7cc0b9d
Yates, Elliott <elliott_yates@harvard.edu>
2017-01-11 11:14:39 -0500

    tlt-1805: publish courses - async_operations.bulk_publish_canvas_sites fixes and cleanup - error details now saved as expected when process completes with status 'failed' - as per comments on PR 193

--- publish_courses/async_operations.py
+++ publish_courses/async_operations.py
@@ -2,15 +2,12 @@
 from __future__ import unicode_literals
 import logging
 
-from django.conf import settings
 from django.utils import timezone
 from rq import get_current_job
 
 from async.models import Process
 from bulk_utilities.bulk_course_settings import BulkCourseSettingsOperation
-from icommons_common.canvas_utils import SessionInactivityExpirationRC
 
-SDK_CONTEXT = SessionInactivityExpirationRC(**settings.CANVAS_SDK_SETTINGS)
 
 logger = logging.getLogger(__name__)
 
@@ -49,7 +46,7 @@ def bulk_publish_canvas_sites(process_id, account=None, course_list=None,
     process.state = Process.ACTIVE
     process.details['rq_job_id'] = getattr(job, 'id', 'None')
     process.details['op_config'] = op_config
-    process.date_active = timezone.now();
+    process.date_active = timezone.now()
     process.save(update_fields=['state', 'details', 'date_active'])
 
     op = BulkCourseSettingsOperation(op_config)
@@ -63,8 +60,8 @@ def bulk_publish_canvas_sites(process_id, account=None, course_list=None,
 
     process.details['stats'] = op.get_stats_dict()
     process.state = Process.COMPLETE
-    process.date_complete = timezone.now();
-    process.save(update_fields=['state', 'status', 'date_complete'])
+    process.date_complete = timezone.now()
+    process.save(update_fields=['state', 'status', 'details', 'date_complete'])
 
     logger.info("Finished bulk_publish_canvas_sites job {}".format(process_id))
     return process
