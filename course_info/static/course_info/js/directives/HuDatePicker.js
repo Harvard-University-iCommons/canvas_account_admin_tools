17158eee49fab391318fe066dcfe7c44a493d865
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2017-06-16 10:06:11 -0400

    Created new date picker directive and partial

---
+++ course_info/static/course_info/js/directives/HuDatePicker.js
@@ -0,0 +1,22 @@
+(function() {
+    var app = angular.module('CourseInfo');
+    app.directive('huDatePicker', huDatePicker);
+
+    function huDatePicker(){
+        return {
+            restrict: 'E',
+            scope: {
+                editable: '<',  // if not provided, defaults to null/false
+                field: '@',
+                formValue: '=',
+                isLoading: '&',
+                label: '@',
+                maxlength: '@',
+                modelValue: '='
+            },
+            templateUrl: 'partials/hu-date-picker.html',
+        };
+    }
+
+});
+
