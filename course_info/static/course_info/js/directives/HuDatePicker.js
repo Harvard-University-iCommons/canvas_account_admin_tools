9881f4009fdea23f9727b3eb044f9e76ac08b898
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2017-06-19 07:07:11 -0400

    Added termConcludeDate to scope and added date picker to link in HuDatePicker

--- course_info/static/course_info/js/directives/HuDatePicker.js
+++ course_info/static/course_info/js/directives/HuDatePicker.js
@@ -2,7 +2,7 @@
     var app = angular.module('CourseInfo');
     app.directive('huDatePicker', huDatePicker);
 
-    function huDatePicker(){
+    function huDatePicker() {
         return {
             restrict: 'E',
             scope: {
@@ -11,11 +11,26 @@
                 formValue: '=',
                 isLoading: '&',
                 label: '@',
-                maxlength: '@',
-                modelValue: '='
+                modelValue: '=',
+                term: '=',
+                termConcludeDate: '='
             },
             templateUrl: 'partials/hu-date-picker.html',
+            link: function (scope, element) {
+                $('#dp-alert').hide();
+                // Create the date picker on the input tag.
+                var inputElement = $(element).find('input');
+                var dp = inputElement.datepicker('setDate', scope.modelValue);
+                dp.change(function() {
+                    var selectedDate = inputElement.val();
+                    // If the selected date is in the past then display alert message and reset the date.
+                    if(Date.parse(selectedDate)-new Date()<0) {
+                        inputElement.val(scope.modelValue);
+                        inputElement.datepicker('setDate', scope.modelValue);
+                        $('#dp-alert').show();
+                    }
+                });
+            }
         };
     }
-
 })();
