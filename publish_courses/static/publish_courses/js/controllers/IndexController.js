b1e850661ecb2503569f3ce4814a4861c6fc2aa8
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2017-07-03 15:11:07 -0400

    Added DataTable and its supporting functions to IndexController

--- publish_courses/static/publish_courses/js/controllers/IndexController.js
+++ publish_courses/static/publish_courses/js/controllers/IndexController.js
@@ -26,6 +26,71 @@
             failure: {
                 alertType: 'danger',
                 text: 'There was a problem publishing these courses.'} };
+        $scope.showDataTable = false;
+        // Canvas course id's that have been selected in the data table
+        $scope.selectedCourses = {};
+        $scope.selectAll = false;
+
+        $scope.addSelectedCourseInstance = function(courseInstanceData){
+            $scope.selectedCourses[courseInstanceData['id']] = courseInstanceData;
+        };
+
+        $scope.removeSelectedCourseInstance = function(courseInstanceData){
+            delete $scope.selectedCourses[courseInstanceData['id']];
+        };
+
+        $scope.getSelectedCourseIdsCount = function(){
+            return Object.keys($scope.selectedCourses).length;
+        };
+
+        $scope.getSelectedCourses = function(){
+            return Object.keys($scope.selectedCourses).map(function(key){
+                return $scope.selectedCourses[key]['id'];
+            })
+        };
+
+        // Return a checkbox used in the first column of the data table, with the course id as its value.
+        $scope.renderSelectionColumn = function(data, type, row, meta){
+            return '<input type="checkbox" class="col-select" data-id="' + data + '"/>';
+        };
+
+        $scope.toggleSelectAll = function(){
+            $scope.selectAll = !$scope.selectAll;
+            $('#courseInfoDT tbody tr').each(function(index, tr){
+                var $checkbox = $(tr).find('.col-select');
+                $checkbox.prop('checked', $scope.selectAll);
+                if ($scope.selectAll) {
+                    $scope.addSelectedCourseInstance($scope.dataTable.row(index).data());
+                } else {
+                    $scope.removeSelectedCourseInstance($scope.dataTable.row(index).data());
+                }
+            });
+        };
+
+        $scope.syncSelectionCheckboxes = function(){
+            $('#courseInfoDT tbody .col-select').each(function(o) {
+                var $checkbox = $(this);
+                var courseInstanceId = $checkbox.data('course_instance_id');
+                var isSelected = Boolean($scope.courseInstanceModel.selectedCourseInstances[courseInstanceId]);
+                $checkbox.closest('tr').toggleClass('selected', isSelected);
+                $checkbox.prop('checked', isSelected);
+            });
+            $scope.selectAll = !$('#courseInfoDT tbody tr').not('.selected').length;
+        };
+
+        $scope.$watch($scope.selectedCourses, $scope.syncSelectionCheckboxes, true);
+
+        $scope.handleRowClick = function(e){
+            var $tr = $(e.target).closest('tr');
+            var selected = !$tr.hasClass('selected');
+            var courseInstanceData = $scope.dataTable.row($tr).data();
+            if (selected) {
+                $scope.addSelectedCourseInstance(courseInstanceData);
+            } else {
+                $scope.removeSelectedCourseInstance(courseInstanceData);
+            }
+            $scope.$apply();
+        };
 
         $scope.filterOutOngoingTerms = function(term) {
           return term.term_code != 99;  // ongoing term
@@ -68,7 +133,7 @@
             var termId = $scope.selectedTerm.meta_term_id;
             // disable Publish button (until user changes term)
             $scope.operationInProgress = true;
-            pcapi.Jobs.create(accountId, termId
+            pcapi.Jobs.createAll(accountId, termId
             ).then(function publishSucceeded(response) {
                 $scope.message = $scope.messages.success;
                 $log.debug(response);
@@ -85,17 +150,83 @@
             // - there are no publishable sites in the term
             // - submitting a job
             // - user has successfully submitted a job
-            return !$scope.selectedTerm
-                   || $scope.coursesSummary.unpublished == 0
-                   || $scope.operationInProgress
-                   || ($scope.message && ($scope.message == $scope.messages.success));
+
+            // todo This is commented out to prevent creation of jobs prior to the UI development story TLT-3144
+            //
+            // return !$scope.selectedTerm
+            //        || $scope.coursesSummary.unpublished == 0
+            //        || $scope.operationInProgress
+            //        || ($scope.message && ($scope.message == $scope.messages.success));
+            return true;
         };
+
+        $scope.publishSelected = function() {
+            $scope.clearMessages();
+            var accountId = $scope.school.id;
+            var termId = $scope.selectedTerm.meta_term_id;
+            // disable Publish button (until user changes term)
+            $scope.operationInProgress = true;
+            pcapi.Jobs.createSelected(accountId, termId, $scope.getSelectedCourses()
+            ).then(function publishSucceeded(response) {
+                $scope.message = $scope.messages.success;
+                $log.debug(response);
+                $log.debug(response.data);
+            }, function publishFailed(response) {
+                $scope.message = $scope.messages.failure;
+            }).finally(function cleanUpAfterPublish() {
+                $scope.operationInProgress = false;
+            });
+        };
+
         $scope.termSelected = function(selectedTerm) {
             $scope.selectedTerm = selectedTerm;
             $scope.clearMessages();
             $scope.loadCoursesSummary();
+            $scope.updateDataTable();
         };
 
         $scope.initialize();
+
+        $scope.updateDataTable = function() {
+            $scope.showDataTable = true;
+            if ($scope.dataTable) {
+                $scope.dataTable.destroy();
+            }
+            var accountId = $scope.school.id;
+            var termId = $scope.selectedTerm.meta_term_id;
+            pcapi.CourseList.get(accountId, termId).then(function (data) {
+                $scope.dataTable = $('#courseInfoDT').DataTable({
+                    retrieve: true,
+                    dom: '<lf<rt>ip>',
+                    language: {
+                        info: 'Showing _START_ to _END_ of _TOTAL_ courses',
+                        emptyTable: 'There are no courses to display.'
+                    },
+                    data: data,
+                    order: [[3, 'asc']],
+                    columns: [
+                        {
+                            data: 'id',
+                            orderable: false,
+                            width: '5%',
+                            className: 'col-align-center',
+                            render: $scope.renderSelectionColumn},
+                        {data: 'id'},
+                        {data: 'course_code'},
+                        {data: 'name'},
+                        {data: 'sis_course_id'},
+                        {data: 'workflow_state'}
+                    ],
+                    preDrawCallback: function(){
+                        $('#courseInfoDT tbody tr').off('click', $scope.handleRowClick);
+                    },
+                    drawCallback: function(){
+                        $scope.syncSelectionCheckboxes();
+                        $scope.$apply();  // otherwise UI selectAll checkbox will not reflect scope changes
+                        $('#courseInfoDT tbody tr').on('click', $scope.handleRowClick);
+                    }
+                });
+            });
+        };
     }
 })();
