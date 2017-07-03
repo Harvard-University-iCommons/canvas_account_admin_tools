ab6ea9fbb21550627a01d6c8af2a8933f754cc41
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2017-07-03 09:26:10 -0400

    Added courseList api view call in PublishCoursesAPIService

--- publish_courses/static/publish_courses/js/services/PublishCoursesAPIService.js
+++ publish_courses/static/publish_courses/js/services/PublishCoursesAPIService.js
@@ -9,10 +9,31 @@
     };
 
     var resources = {
-      jobs: {url: getUrl('api_jobs'), pending: {}},
-      courses: {url: getUrl('api_show_summary'), pending: {}}
+      jobs: {
+        url: getUrl('api_jobs'), pending: {}},
+        courseSummary: {url: getUrl('api_show_summary'), pending: {}},
+        courseList: {url: getUrl('api_course_list'), pending: {}
+      }
     };
 
+    // Returns a list of courses and their details for the given term.
+    var getCourseList = function(accountId, termId) {
+      var config = {
+        params: {
+          account_id: accountId,
+          term_id: termId
+        },
+        logError: {enabled: true, detail: 'fetch course information'},
+        pendingRequestTag: 'courseList'
+      };
+      return $http.get(resources.courseList.url, config
+      ).then(function gotCourseList(response) {
+        return JSON.parse(response.data);
+      });
+    };
+
+    // Returns a summary of courses in the given term.
+    // ie: There are 19 courses in this term; 18 published, 1 unpublished , 0 concluded.
     var getCourseSummary = function(accountId, termId) {
       var config = {
         params: {
@@ -22,7 +43,7 @@
         logError: {enabled: true, detail: 'fetch course summary information'},
         pendingRequestTag: 'courses'
       };
-      return $http.get(resources.courses.url, config
+      return $http.get(resources.courseSummary.url, config
       ).then(function gotCourseSummary(response) {
         return response.data;
       });
@@ -38,8 +59,9 @@
     };
 
     return {
+      CourseList: {get: getCourseList},
       CourseSummary: {get: getCourseSummary},
-      Jobs: {create: createJob}
+      Jobs: {create: createJob},
     };
   }
 })();
