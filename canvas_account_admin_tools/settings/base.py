cf954d437d77559ea43cf2148570e28b47192c8b
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-05-10 14:18:01 -0400

    Removed contrib.postgress from base settings

--- canvas_account_admin_tools/settings/base.py
+++ canvas_account_admin_tools/settings/base.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.postgres',
     'django.contrib.staticfiles',
     'async',
     'bulk_utilities',
