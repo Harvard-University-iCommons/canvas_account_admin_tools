358eb68145c89e10e642ab5a051a59a46b387252
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-07-26 11:26:27 -0400

    Added term_id to form field

--- bulk_course_settings/forms.py
+++ bulk_course_settings/forms.py
@@ -8,4 +8,4 @@ logger = logging.getLogger(__name__)
 class CreateBulkSettingsForm(forms.ModelForm):
     class Meta:
         model = BulkCourseSettingsJob
-        fields = ['setting_to_be_modified', 'desired_setting', 'school_id']
+        fields = ['setting_to_be_modified', 'desired_setting', 'school_id', 'term_id']
