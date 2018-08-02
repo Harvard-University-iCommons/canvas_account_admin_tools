bfefe6151920b697ca9e9b7144fdad9b5325e003
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-08-02 14:14:15 -0400

    Worker command workflow update, tying in util methods

--- bulk_course_settings/management/commands/process_bulk_course_settings_jobs.py
+++ bulk_course_settings/management/commands/process_bulk_course_settings_jobs.py
@@ -1,123 +1,146 @@
-
-import datetime
 import logging
 import signal
-import sys
-import time
-
-
-from botocore.exceptions import ClientError
 
 import boto3
-
+from botocore.exceptions import ClientError
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
+import bulk_course_settings.utils as utils
 from bulk_course_settings.models import BulkCourseSettingsJob
+from icommons_common.models import Term
 
 logger = logging.getLogger(__name__)
-queue_name = settings.BULK_COURSE_SETTINGS['job_queue_name']
-
-
 
 
 class Command(BaseCommand):
     help = 'Process Bulk Course Setting Jobs from queue.'
-    can_import_settings = True
 
     namespaces = {
         'http://icommons.harvard.edu/Schema': None,
     }
 
-    # html_parser = HTMLParser()
-
     def add_arguments(self, parser):
-        parser.add_argument('--dry-run', action='store_true', help="read the file but don't update the database")
-        parser.add_argument('--school-id', help="the school ID is used to determine which queue to watch")
-        parser.add_argument('--job-limit', help="shutdown after processing this many jobs", type=int)
+        parser.add_argument('--job-limit', help="Shutdown after processing this many jobs", type=int)
 
     def handle(self, *args, **options):
-
         signal.signal(signal.SIGTERM, signal_handler)
 
-        aws_region_name = settings.BULK_COURSE_SETTINGS['aws_region_name']
-        aws_access_key_id = settings.BULK_COURSE_SETTINGS['aws_access_key_id']
-        aws_secret_access_key = settings.BULK_COURSE_SETTINGS['aws_secret_access_key']
+        self.aws_region_name = settings.BULK_COURSE_SETTINGS['aws_region_name']
+        self.aws_access_key_id = settings.BULK_COURSE_SETTINGS['aws_access_key_id']
+        self.aws_secret_access_key = settings.BULK_COURSE_SETTINGS['aws_secret_access_key']
+        self.queue_name = settings.BULK_COURSE_SETTINGS['job_queue_name']
+        self.job_limit = options.get('job_limit')
 
-        kw = {'aws_access_key_id': aws_access_key_id,
-              'aws_secret_access_key': aws_secret_access_key,
-              'region_name': aws_region_name
-              }
+        self.kw = {'aws_access_key_id': self.aws_access_key_id,
+                   'aws_secret_access_key': self.aws_secret_access_key,
+                   'region_name': self.aws_region_name
+                   }
 
         try:
-            self.sqs = boto3.resource('sqs', **kw)
+            self.sqs = boto3.resource('sqs', **self.kw)
         except Exception as e:
             logger.exception('Error configuring sqs')
             raise
 
-        self.dry_run = options['dry_run']
-        self.job_limit = options.get('job_limit')
-
-        logger.info('starting a worker for queue %s with a job limit of %d', queue_name, self.job_limit)
-
         try:
-            queue = self.sqs.get_queue_by_name(QueueName=queue_name)
+            self.queue = self.sqs.get_queue_by_name(QueueName=self.queue_name)
         except ClientError:
-            logger.error('failed to get queue %s', queue_name)
-            return False
+            logger.error('Failed to get queue %s', self.queue_name)
+            raise
 
-        # loop indefinitely and get jobs from an SQS queue
-        while True:
-            # use long polling (wait up to 20 seconds) to reduce the number of receive_messages requests we make
-            messages = queue.receive_messages(
-                MaxNumberOfMessages=10,  # TODO: verify what value this should be set to
-                MessageAttributeNames=['All'],
-                AttributeNames=['All'],
-                WaitTimeSeconds=20,
-            )
+        logger.info('Starting a worker for queue %s with a job limit of %d', self.queue_name, self.job_limit)
 
-            if messages:
-                for message in messages:
-                    if message:
-                        self.handle_message(message)
-            else:
-                logger.info('no messages in queue %s', queue_name)
-                time.sleep(40)
+        # WORKFLOW
+        #
+        # get message from the queue
+            # get job record from the database
+            # update the job status to IN_PROGRESS
+            # find all of the courses matching job criteria
+            #     for each course, create a job detail record in the NEW state
+            #     get the current course settings and store in the detail record, change state to IN_PROGRESS
+            #     determine if a change is necessary to match the target value
+            #           if yes, make the API call to update the course in Canvas
+            #               set is_modified to true
+            #               save the new course attributes/settings
+            #               if error, set state of detail record to FAILED (and continue processing courses)
+            #           if no, set is_modified to false
+            #     set workflow state of detail record to COMPLETED_SUCCESS
+            # if all courses processed successfully, update job status to COMPLETED_SUCCESS
+            # else update job status to COMPLETED_ERRORS
+            # delete message from queue
 
-    def handle_message(self, message):
+        # Loop indefinitely and get jobs from an SQS queue
+        # while True:
+        # Use long polling (wait up to 20 seconds) to reduce the number of receive_messages requests we make
+        messages = self.queue.receive_messages(
+            MaxNumberOfMessages=10,  # TODO: verify what value this should be set to
+            MessageAttributeNames=['All'],
+            AttributeNames=['All'],
+            WaitTimeSeconds=20,
+        )
+
+        if messages:
+            for message in messages:
+                self.handle_message(message)
+        else:
+            logger.info('No messages in queue %s', self.queue_name)
+            # time.sleep(40)
+
+    @staticmethod
+    def handle_message(message):
+        # get job record from the database
+        # update the job status to IN_PROGRESS
+        # find all of the courses matching job criteria
+        #     for each course, create a job detail record in the NEW state
+        #     get the current course settings and store in the detail record, change state to IN_PROGRESS
+        #     determine if a change is necessary to match the target value
+        #           if yes, make the API call to update the course in Canvas
+        #               set is_modified to true
+        #               save the new course attributes/settings
+        #               if error, set state of detail record to FAILED (and continue processing courses)
+        #           if no, set is_modified to false
+        #     set workflow state of detail record to COMPLETED_SUCCESS
+        # if all courses processed successfully, update job status to COMPLETED_SUCCESS
+        # else update job status to COMPLETED_ERRORS
+        # delete message from queue
+
         try:
-            logger.info(message)
-            m_attrs = message.attributes
-            print "m_attrs=", m_attrs
-            logger.info('message_attributes: %s', message.message_attributes)
-            logger.info('body: %s', message.body)
-            logger.info('message_id: %s', message.message_id)
             school_id = message.message_attributes['school_id']['StringValue']
             bulk_settings_id = message.message_attributes['bulk_settings_id']['StringValue']
-            logger.info('received message %s for school %s', message.message_id, school_id)
 
             if bulk_settings_id:
-                bulk_course_setting_job = BulkCourseSettingsJob.objects.get(id=bulk_settings_id)
-                bulk_course_setting_job.workflow_status = 'Queued'
-                bulk_course_setting_job.save(update_fields=['workflow_status'])
+                bulk_course_settings_job = BulkCourseSettingsJob.objects.get(id=bulk_settings_id)
+                bulk_course_settings_job.workflow_status = 'IN_PROGRESS'
+                bulk_course_settings_job.save()
 
-                # TODO: Invoke method to process the individual job and populate the job details
+                # TODO
+                # Check if this is a reversion, if so get the list of canvas ID's that need to be reverted
+
+                # Else perform call without course id list
+
+                term = Term.objects.get(term_id=bulk_course_settings_job.term_id)
+
+                canvas_courses = utils.get_canvas_courses(
+                    account_id='sis_account_id:school:{}'.format(bulk_course_settings_job.school_id),
+                    term_id='sis_term_id:{}'.format(term.meta_term_id())
+                )
+
+                for course in canvas_courses:
+                    utils.check_and_update_course(course, bulk_course_settings_job.id)
 
                 logger.info(" Message has been processed , deleting from sqs...")
-                # delete the message from the queue once it is processed
-                if self.dry_run:
-                    # put the message back on the queue
-                    message.change_visibility(VisibilityTimeout=0)
-                else:
-                    # delete the message from the queue so nobody else processes it
-                    logger.debug(" deleting message....")
-                    message.delete()
+                # delete the message from the queue so nobody else processes it
+                logger.debug(" deleting message....")
+                message.delete()
+
+                # TODO Make a check for the associated details and update the workflow_status appropriately
 
         except Exception as e:
             # put the message back on the queue
             logger.exception('Exception caught; re-queueing message %s', message.message_id)
-            bulk_course_setting_job.workflow_status = 'Error'
-            bulk_course_setting_job.save(update_fields=['workflow_status'])
+            # bulk_course_setting_job.workflow_status = 'Error'
+            # bulk_course_setting_job.save(update_fields=['workflow_status'])
             message.change_visibility(VisibilityTimeout=0)
 
         logger.info(" exiting handle_message for bulk_settings_id=" + bulk_settings_id)
