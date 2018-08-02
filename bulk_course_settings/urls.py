652a45a9e90aee7de33e060b54ea8c4de18b5d23
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-08-02 11:46:39 -0400

    URL cleanup

--- bulk_course_settings/urls.py
+++ bulk_course_settings/urls.py
@@ -1,14 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from bulk_course_settings.views import (
-    BulkSettingsListView, BulkSettingsCreateView, BulkSettingsRevertView, add_bulk_job
+    BulkSettingsListView, BulkSettingsCreateView, BulkSettingsRevertView
 )
 
 urlpatterns = [
     url(r'^$', BulkSettingsListView.as_view(), name='bulk_settings_list'),
     url(r'^create_new_setting/$', BulkSettingsCreateView.as_view(), name='create_new_setting'),
     url(r'^revert_setting/$', BulkSettingsRevertView.as_view(), name='revert_setting'),
-
-    url(r'^add_bulk_job/$', add_bulk_job, name='add_bulk_job'),
-
 ]
