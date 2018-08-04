0733063248430d0e8bfb232432e379ff2245af9f
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-08-04 15:45:52 -0400

    Front end reversion handling

--- bulk_course_settings/urls.py
+++ bulk_course_settings/urls.py
@@ -7,5 +7,5 @@ from bulk_course_settings.views import (
 urlpatterns = [
     url(r'^$', BulkSettingsListView.as_view(), name='bulk_settings_list'),
     url(r'^create_new_setting/$', BulkSettingsCreateView.as_view(), name='create_new_setting'),
-    url(r'^revert_setting/$', BulkSettingsRevertView.as_view(), name='revert_setting'),
+    url(r'^revert_setting/(?P<school_id>[a-z]{3,7})/(?P<job_id>[0-9]{1,4})/$', BulkSettingsRevertView.as_view(), name='revert_setting'),
 ]
