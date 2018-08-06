7df4455b57936cb406a62dc3ee1940e1f8a0746a
cms994 <carter_snowden@harvard.edu>
2018-08-06 11:00:17 -0400

    framework for details view — leaving open-ended in light of anticipated model changes

--- bulk_course_settings/urls.py
+++ bulk_course_settings/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from bulk_course_settings.views import (
-    BulkSettingsListView, BulkSettingsCreateView, BulkSettingsRevertView
+
+    BulkSettingsListView, BulkSettingsCreateView, BulkSettingsRevertView, BulkSettingsAuditView
 )
 
 urlpatterns = [
     url(r'^$', BulkSettingsListView.as_view(), name='bulk_settings_list'),
     url(r'^create_new_setting/$', BulkSettingsCreateView.as_view(), name='create_new_setting'),
     url(r'^revert_setting/$', BulkSettingsRevertView.as_view(), name='revert_setting'),
+    url(r'^bulk_settings_job_audit/(?P<parent_job_process_id_id>[0-9]+)$, BulkSettingsAuditView.as_view(), name='bulk_settings_job_audit'),
 ]
--- bulk_course_settings/views.py
+++ bulk_course_settings/views.py
@@ -8,6 +8,7 @@ from django.views.generic.edit import FormView, CreateView
 import utils
 from bulk_course_settings.forms import (CreateBulkSettingsForm)
 from bulk_course_settings.models import BulkCourseSettingsJob
+from bulk_course_settings.models import BulkCourseSettingsJobDetails
 from icommons_common.auth.views import (LoginRequiredMixin)
 
 logger = logging.getLogger(__name__)
@@ -18,6 +19,7 @@ JOB_QUEUE_NAME = settings.BULK_COURSE_SETTINGS['job_queue_name']
 
 def lti_auth_error(request):
     raise PermissionDenied
+
 
 
 class BulkSettingsListView(LoginRequiredMixin, ListView):
@@ -47,15 +49,16 @@ class BulkSettingsCreateView(LoginRequiredMixin, CreateView, FormView):
         return context
 
     def form_valid(self, form):
+
         # If the form is valid, create the BulkCourseSettingsJob and add it to the SQS queue
         form.instance.created_by = str(self.request.user)
         form.instance.updated_by = str(self.request.user)
         response = super(BulkSettingsCreateView, self).form_valid(form)
 
         utils.queue_bulk_settings_job(bulk_settings_id=form.instance.id, school_id=form.instance.school_id,
-                                      term_id=form.instance.term_id,
-                                      setting_to_be_modified=form.instance.setting_to_be_modified,
-                                      desired_setting=form.instance.desired_setting)
+            term_id=form.instance.term_id,
+            setting_to_be_modified=form.instance.setting_to_be_modified,
+            desired_setting=form.instance.desired_setting)
         # TODO check for queue_bulk_settings_job queueing success, if success set queued else completed_failed
         form.instance.workflow_status = 'QUEUED'
         form.instance.save()
@@ -63,5 +66,30 @@ class BulkSettingsCreateView(LoginRequiredMixin, CreateView, FormView):
         return response
 
 
-class BulkSettingsRevertView(LoginRequiredMixin ,CreateView):
+class BulkSettingsRevertView(LoginRequiredMixin, CreateView):
     form_class = ""
+
+
+class BulkSettingsAuditView(LoginRequiredMixin, ListView):
+    model = BulkCourseSettingsJobDetails
+    template_name = 'bulk_course_settings/bulk_settings_job_audit.html'
+    context_object_name = 'bulk_settings_job_audit'
+    queryset = BulkCourseSettingsJobDetails.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(BulkSettingsAuditView, self).get_context_data(**kwargs)
+        pjid = self.request.path.split('/')[3]
+        context['parent_jobid'] = pjid
+        t = BulkCourseSettingsJob.objects.get(id=pjid)
+        context['term_id'] = t.term_id
+        context['parent_job_desired_setting'] = t.desired_setting
+        context['parent_job_workflow_status'] = t.workflow_status
+        context['parent_job_created_by'] = t.created_by
+        context['parent_job_created_at'] = t.created_at
+        context['parent_job_setting_to_be_modified'] = t.setting_to_be_modified
+
+
+
+        return context
+
+
