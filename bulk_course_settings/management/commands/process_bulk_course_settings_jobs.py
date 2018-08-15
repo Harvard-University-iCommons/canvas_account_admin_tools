7b01816086f8d5077fb815e1c899b5ce360a64d4
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2018-08-15 11:10:46 -0400

    Use post state instead of prior state in getting current state value

--- bulk_course_settings/management/commands/process_bulk_course_settings_jobs.py
+++ bulk_course_settings/management/commands/process_bulk_course_settings_jobs.py
@@ -95,7 +95,7 @@ class Command(BaseCommand):
                         # Check to see if the course originally had a None value for the setting to be modified,
                         # Use false as the update arg value in the reversion call.
                         desired_setting = detail.current_setting_value or 'false'
-                        utils.update_course(course=json.loads(detail.prior_state),
+                        utils.update_course(course=json.loads(detail.post_state),
                                             update_args={utils.API_MAPPING[job.setting_to_be_modified]: desired_setting},
                                             job=job)
                 else:
