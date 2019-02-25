9f1cd6a1214b7475af22ef31704549d565653bab
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-02-25 16:46:35 -0500

    Changed the XList filtering

--- cross_list_courses_v2/views.py
+++ cross_list_courses_v2/views.py
@@ -24,14 +24,13 @@ def index(request):
     tool_launch_school = request.LTI['custom_canvas_account_sis_id'].split(':')[1]
 
     # Only get cross listings with current or future terms
-    xlist_maps = XlistMap.objects.filter(
-                    primary_course_instance__term__end_date__gte=today).filter(
-                    secondary_course_instance__term__end_date__gte=today
-                ) | XlistMap.objects.filter(primary_course_instance__term__end_date__isnull=True) | \
-                    XlistMap.objects.filter(secondary_course_instance__term__end_date__isnull=True)
+    xlist_maps = XlistMap.objects.filter(primary_course_instance__term__end_date__gte=today) | \
+                 XlistMap.objects.filter(secondary_course_instance__term__end_date__gte=today) | \
+                 XlistMap.objects.filter(primary_course_instance__term__end_date__isnull=True) | \
+                 XlistMap.objects.filter(secondary_course_instance__term__end_date__isnull=True)
 
     context = {
-        'xlist_maps': xlist_maps[:10],  # Only getting 10 results to temporarily reduce loading time
+        'xlist_maps': xlist_maps,  # Only getting 10 results to temporarily reduce loading time
         'tool_launch_school': tool_launch_school
     }
 
