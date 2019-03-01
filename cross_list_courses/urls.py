ae2dae410ddd140a476d562e0dc6928e176863c0
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-03-01 14:14:58 -0500

    Removed existing cross listing card and code and replaced with new version

--- cross_list_courses/urls.py
+++ cross_list_courses/urls.py
@@ -4,5 +4,4 @@ from cross_list_courses import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^partials/(?P<path>.+)$', views.partials, name='partials'),
 ]
--- cross_list_courses/views.py
+++ cross_list_courses/views.py
@@ -1,12 +1,16 @@
 import logging
+from datetime import datetime
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from lti_permissions.decorators import lti_permission_required
+
 from django_auth_lti import const
 from django_auth_lti.decorators import lti_role_required
+from icommons_common.models import XlistMap
+from lti_permissions.decorators import lti_permission_required
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +20,21 @@ logger = logging.getLogger(__name__)
 @lti_permission_required(settings.PERMISSION_XLIST_TOOL)
 @require_http_methods(['GET'])
 def index(request):
-    return render(request, 'cross_list_courses/index.html')
+    today = datetime.now()
+    # The school that this tool is being launched in
+    tool_launch_school = request.LTI['custom_canvas_account_sis_id'].split(':')[1]
 
-@login_required
-@lti_role_required(const.ADMINISTRATOR)
-@lti_permission_required(settings.PERMISSION_XLIST_TOOL)
-@require_http_methods(['GET'])
-def partials(request, path):
-    return render(request, 'cross_list_courses/partials/' + path, {})
+    # Only get cross listings with current or future terms
+    # Either the primary or secondary courses must be from the school launching the tool
+    xlist_maps = XlistMap.objects.filter(Q(primary_course_instance__term__end_date__gte=today) |
+                                         Q(secondary_course_instance__term__end_date__gte=today) |
+                                         Q(primary_course_instance__term__end_date__isnull=True) |
+                                         Q(secondary_course_instance__term__end_date__isnull=True)).filter(
+                                         Q(primary_course_instance__course__school=tool_launch_school) |
+                                         Q(secondary_course_instance__course__school=tool_launch_school)).select_related()
+
+    context = {
+        'xlist_maps': xlist_maps,
+    }
+
+    return render(request, 'list.html', context=context)
