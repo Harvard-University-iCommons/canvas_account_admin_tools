0e89412a894414efc92dd47433d3dee7bbe83cc8
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-03-05 12:53:40 -0500

    Uses new icommons-common branch to lookup a Person and display their full name

--- cross_list_courses/views.py
+++ cross_list_courses/views.py
@@ -31,8 +31,7 @@ def index(request):
                                          Q(primary_course_instance__term__end_date__isnull=True) |
                                          Q(secondary_course_instance__term__end_date__isnull=True)).filter(
                                          Q(primary_course_instance__course__school=tool_launch_school) |
-                                         Q(secondary_course_instance__course__school=tool_launch_school)).select_related('primary_course_instance',
-                                                                                                                         'secondary_course_instance')[:10]
+                                         Q(secondary_course_instance__course__school=tool_launch_school)).select_related()
 
     context = {
         'xlist_maps': xlist_maps,
