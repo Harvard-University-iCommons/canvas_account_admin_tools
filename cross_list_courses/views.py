5598881efca9347fde8f2da796f0c13db8f1c9ff
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-03-05 11:22:03 -0500

    Added more CI fields to the table

--- cross_list_courses/views.py
+++ cross_list_courses/views.py
@@ -32,7 +32,7 @@ def index(request):
                                          Q(secondary_course_instance__term__end_date__isnull=True)).filter(
                                          Q(primary_course_instance__course__school=tool_launch_school) |
                                          Q(secondary_course_instance__course__school=tool_launch_school)).select_related('primary_course_instance',
-                                                                                                                         'secondary_course_instance')
+                                                                                                                         'secondary_course_instance')[:10]
 
     context = {
         'xlist_maps': xlist_maps,
