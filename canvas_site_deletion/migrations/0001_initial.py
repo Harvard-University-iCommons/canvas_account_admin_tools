c58cfa33575a5ad74ecec698a4ecdd2f5d6b7c43
Sapna Mysore <sapna_mysore@harvard.edu>
2019-04-23 12:11:07 -0400

    New Card restricted to Account Admins, to cleanup Canvas sites as per https://jira.huit.harvard.edu/browse/TLT-1955

---
+++ canvas_site_deletion/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+         ('lti_permissions', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="INSERT INTO lti_permissions_ltipermission "
+                  "(permission, school_id, canvas_role, allow) "
+                  "VALUES ('canvas_site_deletion', '*', 'AccountAdmin', '1'),"
+                  "('canvas_site_deletion', '*', 'Account admin', '1'),"
+                  "('canvas_site_deletion', '*', 'Account Admin', '1');",
+            reverse_sql="delete from lti_permissions_ltipermission where permission='cross_listing';",
+        ),
+    ]
+
