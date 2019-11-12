4e5e5efd3a106333cbd7a737ec2de49159809f6d
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-12 17:06:23 -0500

    Add interceptor

--- publish_courses/static/publish_courses/js/app.js
+++ publish_courses/static/publish_courses/js/app.js
@@ -11,7 +11,24 @@
        'ngRoute',
        'ngSanitize',
        'ui.bootstrap']);
-    app.config(['$routeProvider', function ($routeProvider){
+    app.config(['$routeProvider', function ($httpProvider, $routeProvider){
+        $httpProvider.interceptors.push(function(){
+            return {
+                'request': function(config){
+                    // Append LTI resource link ID to all AJAX requests not
+                    // triggered by ui-bootstrap.  Those will be served from
+                    // the template cache, but only if we don't screw with the
+                    // url.
+                    //
+                    // window.globals.append_resource_link_id function added by
+                    // django_auth_lti/js/resource_link_id.js
+                    if (!config.url.startsWith('uib/template/')) {
+                        config.url = window.globals.append_resource_link_id(config.url);
+                    }
+                    return config;
+                }
+            };
+        });
         $routeProvider
             .when('/', {
                 templateUrl: 'partials/list.html',
@@ -20,5 +37,7 @@
             .otherwise({
                 redirectTo: '/',
             });
+
+
     }]);
 })();
