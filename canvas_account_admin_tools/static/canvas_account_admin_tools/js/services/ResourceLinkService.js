a5181ee87aa0b5de65549fbef61bd5da791839b1
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-13 15:11:13 -0500

    Tweaked ResourceLinkService

--- canvas_account_admin_tools/static/canvas_account_admin_tools/js/services/ResourceLinkService.js
+++ canvas_account_admin_tools/static/canvas_account_admin_tools/js/services/ResourceLinkService.js
@@ -1,14 +1,6 @@
 (function() {
   var module = angular.module('resourceLinkModule', []);
   var resourceLinkInterceptor = function(DjangoContextData) {
-    var appendResourceLinkId = function(url){
-      if (!url.match(/resource_link_id/)) {
-        var url_separator = (url.match(/\?/)) ? '&' : '?';
-        return url + url_separator + 'resource_link_id='
-          + DjangoContextData.resource_link_id;
-      }
-      return url;
-    };
     return {
       'request': function (config) {
         // Append LTI resource link ID to all AJAX requests not
@@ -19,7 +11,7 @@
         // window.globals.append_resource_link_id function added by
         // django_auth_lti/js/resource_link_id.js
         if (!config.url.startsWith('uib/template/')) {
-          config.url = appendResourceLinkId(config.url);
+          config.url = window.globals.append_resource_link_id(config.url);
         }
         return config;
       }
