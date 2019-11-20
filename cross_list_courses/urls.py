1f6b4d33a5b97da155daf6e7fe270e7df6b39c38
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-20 15:25:24 -0500

    Test removing slash from get_ci_data

--- cross_list_courses/urls.py
+++ cross_list_courses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     re_path('add_new_pair/', views.add_new_pair, name='add_new_pair'),
     re_path('create_new_pair/', views.create_new_pair, name='create_new_pair'),
     re_path(r'^delete_cross_listing/(?P<pk>\d+)/', views.delete_cross_listing, name='delete_cross_listing'),
-    re_path('get_ci_data/', views.get_ci_data, name='get_ci_data'),
+    re_path('get_ci_data', views.get_ci_data, name='get_ci_data'),
 ]
