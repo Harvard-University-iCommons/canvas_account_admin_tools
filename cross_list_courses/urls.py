a7b3eaa9ec6b75cc043844c6fb0400b98663fe85
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-20 15:30:36 -0500

    add_new_pair/get_ci_data url pattern

--- cross_list_courses/urls.py
+++ cross_list_courses/urls.py
@@ -4,8 +4,8 @@ from cross_list_courses import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path('add_new_pair/get_ci_data', views.get_ci_data, name='get_ci_data'),
     re_path('add_new_pair/', views.add_new_pair, name='add_new_pair'),
     re_path('create_new_pair/', views.create_new_pair, name='create_new_pair'),
     re_path(r'^delete_cross_listing/(?P<pk>\d+)/', views.delete_cross_listing, name='delete_cross_listing'),
-    re_path('get_ci_data', views.get_ci_data, name='get_ci_data'),
 ]
