d10ab7a781e46d1aebce6c1f6b8cacbfcb837ee2
Sapna Mysore <sapna_mysore@harvard.edu>
2021-02-01 15:56:23 -0500

    Added logging, and  confirmation page with timestamp

--- masquerade_tool/views.py
+++ masquerade_tool/views.py
@@ -2,6 +2,7 @@ import logging
 import boto3
 import json
 
+from datetime import datetime,timedelta
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -27,16 +28,29 @@ def index(request):
 @lti_permission_required(settings.PERMISSION_MASQUERADE_TOOL)
 @require_http_methods(['GET', 'POST'])
 def add_role(request):
-    canvas_user_id = request.LTI['custom_canvas_user_id']
-    lambda_client = boto3.client('lambda')
+    context = {}
+    try:
+        canvas_user_id = request.LTI['custom_canvas_user_id']
+        now = datetime.now()
+        expiry_time = datetime.now() + timedelta(hours=1)
+        formatted_time = format(expiry_time, '%A %Y-%m-%d %H:%M:%S')
+        logger.info('User {} requested masqeurade access at {}, for one hour until {}'.format(
+            request.user, now, formatted_time ))
 
-    payload = {
-        'user_id': canvas_user_id,
-        'action': 'add',
-    }
-    result = lambda_client.invoke(
-        FunctionName='arn:aws:lambda:us-east-1:482956169056:function:temporary-masquerade-dev-TemporaryMasqueradeFuncti-1IECMNSLQXP1X',
-        Payload=json.dumps(payload),
-    )
-    print(result)
-    return render(request, 'masquerade_tool/index.html')
+        expiry_time
+        lambda_client = boto3.client('lambda')
+        payload = {
+            'user_id': canvas_user_id,
+            'action': 'add',
+        }
+        result = lambda_client.invoke(
+            FunctionName='arn:aws:lambda:us-east-1:482956169056:function:temporary-masquerade-dev-TemporaryMasqueradeFuncti-1IECMNSLQXP1X',
+            Payload=json.dumps(payload),
+        )
+        logger.info(result)
+        status = result['ResponseMetadata']['HTTPStatusCode']
+        context = {'expiry_time': formatted_time}
+    except Exception as e:
+        logger.error(e)
+
+    return render(request, 'masquerade_tool/success.html', context)
