b7f0b07ca8baae4776d5fd6ff36b449744446918
Sapna Mysore <sapna_mysore@harvard.edu>
2021-02-02 14:36:23 -0500

    Changed the message and also handled the case where user already has this role

--- masquerade_tool/views.py
+++ masquerade_tool/views.py
@@ -3,7 +3,9 @@ import boto3
 import json
 import dateutil.parser
 
-from datetime import datetime,timedelta
+from icommons_common.canvas_utils import SessionInactivityExpirationRC
+
+from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -12,8 +14,8 @@ from django.views.decorators.http import require_http_methods
 from django_auth_lti import const
 from django_auth_lti.decorators import lti_role_required
 from lti_permissions.decorators import lti_permission_required
-logger = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 
 @login_required
 @lti_role_required(const.ADMINISTRATOR)
@@ -32,8 +34,8 @@ def add_role(request):
     context = {}
     try:
         canvas_user_id = request.LTI['custom_canvas_user_id']
-        logger.info('User {} requested masqeurade access at {}'.format(
-            request.user, datetime.now()))
+        logger.info('User {} requested masquerade access at {}'.format(
+            request.LTI['lis_person_name_full'], datetime.now()))
 
         # TODO: move the region_name to SSM
         lambda_client = boto3.client('lambda', region_name='us-east-1')
@@ -47,11 +49,18 @@ def add_role(request):
             FunctionName='arn:aws:lambda:us-east-1:482956169056:function:temporary-masquerade-dev-TemporaryMasqueradeFuncti-1IECMNSLQXP1X',
             Payload=json.dumps(payload),
         )
-        status = result['ResponseMetadata']['HTTPStatusCode']
+        logger.info('lambda result = {}'.format(result))
+        invoke_status = result['ResponseMetadata']['HTTPStatusCode']
         response_payload = json.loads(result['Payload'].read().decode("utf-8"))
-        exp_dt = dateutil.parser.isoparse(response_payload["expires"])
-        logger.info('lambda result = {},  payload response =  {}'.format(result, response_payload))
-        context = {'expiry_time': exp_dt}
+
+        status = response_payload["status"]
+        exp_dt = None
+        # Check if expires is being sent. if role already exists, expires won't be set.
+        if "expires" in response_payload:
+            exp_dt = dateutil.parser.isoparse(response_payload["expires"])
+
+        logger.info(' payload response =  {}'.format( response_payload))
+        context = {'expiry_time': exp_dt, 'status': status}
     except Exception as e:
         logger.error(e)
 
