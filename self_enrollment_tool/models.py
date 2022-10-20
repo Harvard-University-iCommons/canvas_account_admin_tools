b3e8c55f8666eab9c27c7b5ea057162a3a6051c7
Patrick Tonne <patrick_tonne@harvard.edu>
2022-10-20 02:39:56 -0400

    Add uuid field to SelfEnrollmentCourse; add unique_together constraint on course_instance_id and role_id to SelfEnrollmentCourse

--- self_enrollment_tool/models.py
+++ self_enrollment_tool/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from uuid import uuid4
 
 
 class SelfEnrollmentCourse(models.Model):
 
-    course_instance_id = models.IntegerField(unique=True)
-    role_id = models.IntegerField()
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    uuid = models.UUIDField(default=uuid4, unique=True)
+    course_instance_id = models.IntegerField()
+    role_id = models.CharField(max_length=20)
     updated_by = models.CharField(max_length=15)
     last_updated = models.DateTimeField(auto_now_add=True)
+
     class Meta:
+        unique_together = ('course_instance_id', 'role_id',)
         db_table = 'self_enrollment_course'
-        
