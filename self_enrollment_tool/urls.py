f976a73a90d14a2197db558d092e49d759001d9c
Sapna Mysore <sapna_mysore@harvard.edu>
2022-10-20 10:30:04 -0400

    - Moved the creation of new  self enrollment records into a new screen from  the initial screen - Added validation to check for existing self enrollment records before lookup

--- self_enrollment_tool/urls.py
+++ self_enrollment_tool/urls.py
@@ -4,6 +4,7 @@ from self_enrollment_tool import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_new/', views.add_new, name='add_new'),
     path('lookup/', views.lookup, name='lookup'),
     path('enable/(?P<course_instance_id>\d+)$', views.enable,  name='enable'),
     path('enroll/(?P<course_instance_id>\d+)$', views.enroll, name='enroll'),
