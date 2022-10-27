48f33d9844cc061c2a1da19cd8fcd02a1e30d29c
Patrick Tonne <patrick_tonne@harvard.edu>
2022-10-27 16:21:42 -0400

    Update /enroll path to require a UUID

--- self_enrollment_tool/urls.py
+++ self_enrollment_tool/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('add_new/', views.add_new, name='add_new'),
     path('lookup/', views.lookup, name='lookup'),
     path('enable/<int:course_instance_id>/', views.enable,  name='enable'),
-    path('enroll/<int:course_instance_id>/', views.enroll, name='enroll'),
+    path('enroll/<uuid:uuid>', views.enroll, name='enroll'),
     path('disable/<int:pk>/',
          views.disable, name='disable'),
     path('unenroll_user_from_course/<int:course_instance_id>/<int:user_id>',
