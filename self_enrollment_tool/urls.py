d269370b8d1f705b762a69bb0eb135306d3c515f
Colin Murtaugh <cmurtaugh@gmail.com>
2022-12-12 13:04:19 -0500

    removed the 'enroll' URL since this is now handled by a separate Lambda function / SAM app

--- self_enrollment_tool/urls.py
+++ self_enrollment_tool/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('add_new/', views.add_new, name='add_new'),
     path('lookup/', views.lookup, name='lookup'),
     path('enable/<int:course_instance_id>/', views.enable,  name='enable'),
-    path('enroll/<uuid:uuid>', views.enroll, name='enroll'),
     path('disable/<uuid:uuid>/', views.disable, name='disable'),
 ]
