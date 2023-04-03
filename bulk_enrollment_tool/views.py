2611c7bfe7baedf930def8ba459ebe8f7da783bd
Haydn <haydn_ady@harvard.edu>
2023-04-03 12:07:06 -0400

    views file initial code

--- bulk_enrollment_tool/views.py
+++ bulk_enrollment_tool/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
+from django_auth_lti import const
+from django_auth_lti.decorators import lti_role_required
+from lti_school_permissions.decorators import lti_permission_required
+
+logger = logging.getLogger(__name__)
+
+
+@login_required
+@lti_role_required(const.ADMINISTRATOR)
+@lti_permission_required(settings.PERMISSION_BULK_ENROLLMENT_TOOL)
+@require_http_methods(['GET'])
+def index(request):
+    context = {
+        'message': 'Hello!!!'
+    }
+    return render(request, 'bulk_enrollment_tool/index.html', context=context)
+
+
+@login_required
+@lti_role_required(const.ADMINISTRATOR)
+@lti_permission_required(settings.PERMISSION_BULK_ENROLLMENT_TOOL)
+@require_http_methods(['GET'])
+def create_dynamodb_record() -> None:
+    """
+    Creates bulk enrollment record in DynamoDB table.
+    """
+    return None
+
+
+@login_required
+@lti_role_required(const.ADMINISTRATOR)
+@lti_permission_required(settings.PERMISSION_BULK_ENROLLMENT_TOOL)
+@require_http_methods(['GET'])
+def store_file_in_s3() -> None:
+    """
+    Stores user bulk enrollment uploaded file in S3 bucket.
+    """
+    return None
