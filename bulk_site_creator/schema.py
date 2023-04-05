a07a2555f677e1c3acd8051dfda5fee5f5a27476
Patrick Tonne <patrick_tonne@harvard.edu>
2023-04-05 13:52:24 -0400

    Update Job/Task schema

--- bulk_site_creator/schema.py
+++ bulk_site_creator/schema.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import List, Optional
 
 from ulid import ULID
@@ -6,6 +7,7 @@ from ulid import ULID
 Defines the schema of the dynamodb table.
 """
 
+
 class JobRecord:
     def __init__(
         self,
@@ -13,20 +15,27 @@ class JobRecord:
         sis_term_id: int,
         sis_department_id: Optional[int],
         sis_course_group_id: Optional[int],
-        template_canvas_course_id: int,
         created_by_user_id: str,
         course_instance_ids: List[str],
-        status: str
+        workflow_state: str,
+        creator_email: str,
+        user_full_name: str,
+        template_name: Optional[str],
+        template_id: Optional[int],
     ):
         self.pk = f"SCHOOL#{school}"
         self.sk = f"JOB#{str(ULID)}"
         self.sis_term_id = sis_term_id
         self.sis_department_id = sis_department_id
         self.sis_course_group_id = sis_course_group_id
-        self.template_canvas_course_id = template_canvas_course_id
         self.created_by_user_id = created_by_user_id
         self.course_instance_ids = course_instance_ids
-        self.status = status
+        self.workflow_state = workflow_state
+        self.created_at = datetime.datetime.now()
+        self.creator_email = creator_email
+        self.user_full_name = user_full_name
+        self.template_name = template_name
+        self.template_id = template_id
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -38,29 +47,30 @@ class JobRecord:
             "sis_term_id": self.sis_term_id,
             "sis_department_id": self.sis_department_id,
             "sis_course_group_id": self.sis_course_group_id,
-            "template_canvas_course_id": self.template_canvas_course_id,
             "created_by_user_id": self.created_by_user_id,
             "course_instance_ids": self.course_instance_ids,
-            "status": self.status
+            "workflow_state": self.workflow_state,
         }
 
 
 class TaskRecord:
     def __init__(
-        self, job_record: JobRecord, sis_course_id: str, created_by_user_id: str, status: str
+        self,
+        job_record: JobRecord,
+        workflow_state: str,
+        created_by_user_id: str,
     ):
         self.pk = job_record.sk
         self.sk = f"TASK#{str(ULID)}"
-        self.sis_course_id = sis_course_id
-        self.created_by_user_id = created_by_user_id,
-        self.status = status
+        self.workflow_state = workflow_state
+        self.created_by_user_id = created_by_user_id
+        self.created_at = datetime.datetime.now()
 
     def to_dict(self):
         return {
             "pk": self.pk,
             "sk": self.sk,
-            "sis_course_id": self.sis_course_id,
             "created_by_user_id": self.created_by_user_id,
-            "status": self.status
+            "workflow_state": self.workflow_state,
+            "created_at": self.created_at,
         }
-
