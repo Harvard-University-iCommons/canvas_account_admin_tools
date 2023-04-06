2a22188567674effaaf0b732185a94403a9bea63
Patrick Tonne <patrick_tonne@harvard.edu>
2023-04-06 00:22:14 -0400

    Update schema models to incorporate additional data + some validation

--- bulk_site_creator/schema.py
+++ bulk_site_creator/schema.py
@@ -1,4 +1,5 @@
 import datetime
+from enum import Enum
 from typing import List, Optional
 
 from ulid import ULID
@@ -7,35 +8,42 @@ from ulid import ULID
 Defines the schema of the dynamodb table.
 """
 
-
 class JobRecord:
     def __init__(
         self,
         school: str,
-        sis_term_id: int,
-        sis_department_id: Optional[int],
-        sis_course_group_id: Optional[int],
-        created_by_user_id: str,
-        course_instance_ids: List[str],
-        workflow_state: str,
-        creator_email: str,
-        user_full_name: str,
+        term_id: str,
+        term_name: Optional[str],
+        department_id: Optional[str],
+        department_name: Optional[str],
+        course_group_id: Optional[str],
+        course_group_name: Optional[str],
+        template_id: Optional[str],
         template_name: Optional[str],
-        template_id: Optional[int],
+        user_id: str,
+        user_full_name: str,
+        user_email: str,
+        workflow_state: str,
     ):
-        self.pk = f"SCHOOL#{school}"
+        self.pk = f"SCHOOL#{school.upper()}"
         self.sk = f"JOB#{str(ULID)}"
-        self.sis_term_id = sis_term_id
-        self.sis_department_id = sis_department_id
-        self.sis_course_group_id = sis_course_group_id
-        self.created_by_user_id = created_by_user_id
-        self.course_instance_ids = course_instance_ids
-        self.workflow_state = workflow_state
-        self.created_at = datetime.datetime.now()
-        self.creator_email = creator_email
-        self.user_full_name = user_full_name
-        self.template_name = template_name
+        self.term_id = term_id
+        self.term_name = term_name
+        self.department_id = department_id
+        self.department_name = department_name
+        self.course_group_id = course_group_id
+        self.course_group_name = course_group_name
         self.template_id = template_id
+        self.template_name = template_name
+        self.user_id = user_id
+        self.user_full_name = user_full_name
+        self.user_email = user_email
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+        if not is_valid_state(workflow_state):
+            raise ValueError(f"Invalid workflow state: {workflow_state}")
+        self.workflow_state = workflow_state.upper()
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -44,12 +52,19 @@ class JobRecord:
         return {
             "pk": self.pk,
             "sk": self.sk,
-            "sis_term_id": self.sis_term_id,
-            "sis_department_id": self.sis_department_id,
-            "sis_course_group_id": self.sis_course_group_id,
-            "created_by_user_id": self.created_by_user_id,
-            "course_instance_ids": self.course_instance_ids,
+            "term_id": self.term_id,
+            "department_id": self.department_id,
+            "department_name": self.department_name,
+            "course_group_id": self.course_group_id,
+            "course_group_name": self.course_group_name,
+            "template_name": self.template_name,
+            "template_id": self.template_id,
+            "user_id": self.user_id,
+            "user_full_name": self.user_full_name,
             "workflow_state": self.workflow_state,
+            "user_email": self.user_email,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
 
 
@@ -57,20 +72,33 @@ class TaskRecord:
     def __init__(
         self,
         job_record: JobRecord,
+        course_instance_id: str,
         workflow_state: str,
-        created_by_user_id: str,
     ):
         self.pk = job_record.sk
         self.sk = f"TASK#{str(ULID)}"
-        self.workflow_state = workflow_state
-        self.created_by_user_id = created_by_user_id
+        self.course_instance_id = course_instance_id
         self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+        if not is_valid_state(workflow_state):
+            raise ValueError(f"Invalid workflow state: {workflow_state}")
+        self.workflow_state = workflow_state.upper()
 
     def to_dict(self):
         return {
             "pk": self.pk,
             "sk": self.sk,
-            "created_by_user_id": self.created_by_user_id,
             "workflow_state": self.workflow_state,
             "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
+
+
+class States(Enum):
+    PENDING = "PENDING"
+    IN_PROGRESS = "IN_PROGRESS"
+    COMPLETE = "COMPLETE"
+
+def is_valid_state(workflow_state: str):
+    return workflow_state.upper() in [state.value for state in States]
