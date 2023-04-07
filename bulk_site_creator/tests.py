fbd6ceede0a567da9f11140eef955e77f2c067e9
Patrick Tonne <patrick_tonne@harvard.edu>
2023-04-07 10:15:33 -0400

    Udate CreateBulkJobTestCase

--- bulk_site_creator/tests.py
+++ bulk_site_creator/tests.py
@@ -1,34 +1,48 @@
 import json
+from unittest.mock import Mock, patch
 
-from django.test import Client, TestCase
-from django.urls import reverse
+from django.test import Client, RequestFactory, TestCase
+
+from .views import create_bulk_job
 
 
-class BulkCreateJobTestCase(TestCase):
+@patch('lti_permissions.decorators.lti_permission_required_check', new=Mock(return_value=True))
+@patch('django_auth_lti.decorators.is_allowed', new=Mock(return_value=True))
+@patch('lti_permissions.decorators.is_allowed', new=Mock(return_value=True))
+class CreateBulkJobTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.url = reverse("create_bulk_job")
+        self.Client = Client()
+        self.url = '/bulk_site_creator/create_bulk_job'
         self.data = {
             "canvas_user_id": 1,
             "logged_in_user_id": 2,
             "filters": {
-                "term": "2018-summer",
-                "school": "colgsas",
-                "department": "anthropology",
-                "course_group": 1,
+                "term": "10571",
+                "department": "dept:1864",
+                "course_group": "",
+                "school": "school:hds",
             },
             "course_instance_ids": [1, 2, 3],
+            "create_all": False,
+            "template": "2550"
         }
+        self.json_data = json.dumps(self.data)
 
-    def test_bulk_create_job_post(self):
-        # Make a POST request to the view with the data
-        response = self.client.post(
-            self.url, data=json.dumps(self.data), content_type="application/json"
-        )
+    @patch('bulk_site_creator.utils.get_term_data')
+    @patch('bulk_site_creator.views.get_term_name_by_id')
+    @patch('bulk_site_creator.views.get_department_name_by_id')
+    def test_create_bulk_job(self, mock_department, mock_get_term_name_by_id, mock_get_term_data):
+        mock_department.return_value = "OMS"
+        mock_get_term_data.return_value = {'id': "10571", 'name': "Spring 2021"}
+        mock_get_term_name_by_id.return_value = 'Spring 2021'
 
-        # Assert that the response has a 200 OK status code
+        self.request = RequestFactory().post(self.url, data=self.json_data, content_type='application/json')
+        self.request.LTI = {
+            "lis_person_sourcedid": "12345678",
+            "lis_person_name_full": "First Last",
+            "lis_person_contact_email_primary": "test@test.com",
+        }
+        self.request.user = Mock(is_authenticated=Mock(return_value=True))
+        response = create_bulk_job(self.request)
+
         self.assertEqual(response.status_code, 200)
-
-        # Assert that the response content is what you expect
-        expected_response = {"status": "success"}
-        self.assertEqual(json.loads(response.content), expected_response)
