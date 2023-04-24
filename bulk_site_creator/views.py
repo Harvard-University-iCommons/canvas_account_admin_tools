c526352701dd52f056c13e605ec9802e22c1027a
Haydn <haydn_ady@harvard.edu>
2023-04-23 21:14:56 -0400

    WIP

--- bulk_site_creator/views.py
+++ bulk_site_creator/views.py
@@ -113,8 +113,6 @@ def new_job(request):
     selected_department_id = None
 
     # Only display the Course Groups dropdown if the tool is launched in the COLGSAS sub-account
-    print('request ------------------------------>', request.POST)
-    print('school_id ------------------------------>', school_id)
     if school_id == 'colgsas':
         try:
             course_groups = get_course_group_data_for_school(sis_account_id)
@@ -127,6 +125,9 @@ def new_job(request):
         except Exception:
             logger.exception(f"Failed to get departments with sis_account_id {sis_account_id}")
 
+    print('request ------------------------------>', request.POST)
+    print('school_id ------------------------------>', school_id)
+    
     if request.method == "POST":
         selected_term_id = request.POST.get("courseTerm", None)
         selected_course_group_id = request.POST.get("courseCourseGroup", None)
@@ -135,13 +136,6 @@ def new_job(request):
         # Retrieve all course instances for the given term_id and account that do not have Canvas course sites
         # nor are set to be fed into Canvas via the automated feed
         # TODO Add bulk_processing flag to filter
-        # if school_id == 'colgsas':
-        #     if selected_course_group_id == '0':
-        #         print('selected_term_id ------------------------------>', selected_term_id)
-        #     else:
-        #         print('selected_term_id, selected_department_id------------------------------>', selected_term_id, selected_course_group_id.split(":")[1])
-        # else:
-        #     print('selected_term_id, selected_department_id------------------------------>', selected_term_id, selected_department_id)
         if selected_course_group_id or selected_department_id:
             if selected_course_group_id == '0' or selected_department_id == '0':
                 potential_course_sites_query = get_course_instance_query_set(
@@ -164,13 +158,7 @@ def new_job(request):
                          term__term_id=selected_term_id,
                          course__departments=selected_department_id.split(":")[1])
 
-            # potential_course_sites_query = get_course_instance_query_set(
-            #     selected_term_id, sis_account_id
-            # ).filter(canvas_course_id__isnull=True, sync_to_canvas=0)
-
             print('potential_course_sites_query ------------------------------>', potential_course_sites_query)
-            print('selected_department_id ------------------------------>', selected_department_id)
-            print('selected_course_group_id ------------------------------>', selected_course_group_id)
 
 
     # TODO maybe better to use template tag unless used elsewhere?
