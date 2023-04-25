036fc7dd7b225783636f9741845c57fb280c0af3
Haydn <haydn_ady@harvard.edu>
2023-04-25 12:23:21 -0400

    Minor formatting change for easier reading

--- bulk_site_creator/views.py
+++ bulk_site_creator/views.py
@@ -130,10 +130,8 @@ def new_job(request):
     
     if request.method == "POST":
         selected_term_id = request.POST.get("courseTerm", None)
-        selected_course_group_id = request.POST.get("courseCourseGroup").split(
-            ":")[1] if request.POST.get("courseCourseGroup", None) else None
-        selected_department_id = request.POST.get("courseDepartment").split(
-            ":")[1] if request.POST.get("courseDepartment", None) else None
+        selected_course_group_id = request.POST.get("courseCourseGroup").split(":")[1] if request.POST.get("courseCourseGroup", None) else None
+        selected_department_id = request.POST.get("courseDepartment").split(":")[1] if request.POST.get("courseDepartment", None) else None
         
         # TODO Add bulk_processing flag to filter
 
@@ -147,12 +145,10 @@ def new_job(request):
 
         # Filter potential_course_sites_query by course group.
         if selected_course_group_id and selected_course_group_id != '0':
-            potential_course_sites_query = potential_course_sites_query.filter(
-                course__course_group=selected_course_group_id)
+            potential_course_sites_query = potential_course_sites_query.filter(course__course_group=selected_course_group_id)
         # Filter potential_course_sites_query by department.
         elif selected_department_id and selected_department_id != '0':
-            potential_course_sites_query = potential_course_sites_query.filter(
-                course__department=selected_department_id)
+            potential_course_sites_query = potential_course_sites_query.filter(course__department=selected_department_id)
 
     # TODO maybe better to use template tag unless used elsewhere?
     # TODO cont. this may be included in a summary generation to be displayed in page (see wireframe and Jira ticket)
