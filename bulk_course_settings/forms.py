529c0508b1564a68ab2962d5bc267b04786b9c5b
Chris Thornton <>
2023-08-29 11:54:25 -0400

    Bulk Course Settings template updates

--- bulk_course_settings/forms.py
+++ bulk_course_settings/forms.py
@@ -8,6 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 class CreateBulkSettingsForm(forms.ModelForm):
-    class Meta:
-        model = Job
-        fields = ['setting_to_be_modified', 'desired_setting', 'school_id', 'term_id']
+	class Meta:
+		model = Job
+		fields = ['setting_to_be_modified', 'desired_setting', 'term_id']
+
+	def __init__(self, *args, **kwargs):
+		super(CreateBulkSettingsForm, self).__init__(*args, **kwargs)
+		for field in self.fields.values():
+			field.widget.attrs['class'] = 'form-control'
