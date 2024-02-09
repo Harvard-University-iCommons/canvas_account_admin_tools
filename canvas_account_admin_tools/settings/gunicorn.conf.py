78f6c04b8d8ce0d198bd494c14ab206dc543a88f
Haydn <haydn_ady@harvard.edu>
2024-02-09 11:07:32 -0500

    Added 55 seconds gunicorn timeout

--- canvas_account_admin_tools/settings/gunicorn.conf.py
+++ canvas_account_admin_tools/settings/gunicorn.conf.py
@@ -7,3 +7,6 @@ workers = 3
 accesslog = '-'
 errorlog = '-'
 access_log_format = '{"request": "%(r)s", "http_status_code": "%(s)s", "http_request_url": "%(U)s", "http_query_string": "%(q)s", "http_verb": "%(m)s", "http_version": "%(H)s", "http_referer": "%(f)s", "x_forwarded_for": "%({x-forwarded-for}i)s", "remote_address": "%(h)s", "request_usec": "%(D)s", "request_sec": "%(L)s"}'
+
+# Timeout setting (in seconds).
+timeout = 55
