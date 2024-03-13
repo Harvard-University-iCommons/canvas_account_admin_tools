5e4596c08be863b3f1f0e94edbeb167b72799dfc
Haydn <haydn_ady@harvard.edu>
2024-03-13 11:14:16 -0400

    Added logging configs, updated views and urls

--- course_info_v2/views.py
+++ course_info_v2/views.py
@@ -1,5 +1,13 @@
+import logging
+
 from django.shortcuts import render
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    pass
+    return render(request, "course_info_v2/course_search.html", {})
+
+
+def details(request):
+    return render(request, "course_info_v2/course_details.html", {})
