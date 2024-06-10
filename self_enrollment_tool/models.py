a9d8f3ce48298c79ff766590faee987b48a76228
Jordan Chu <jordanchu95@gmail.com>
2024-06-10 08:11:48 -0400

    adding start and ed date to self enrollment course model

--- self_enrollment_tool/models.py
+++ self_enrollment_tool/models.py
@@ -10,6 +10,8 @@ class SelfEnrollmentCourse(models.Model):
     role_id = models.CharField(max_length=20)
     updated_by = models.CharField(max_length=15)
     last_updated = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         unique_together = ('course_instance_id', 'role_id',)
